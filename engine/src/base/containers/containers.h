723c6b3b4f71c038019eae2e745a5c74ddeaa5e5
Aby Louw <jalouw@csir.co.za>
2010-01-05 12:39:00 +0200

    Initial commit of src/base/containers header file

---
+++ engine/src/base/containers/containers.h
@@ -0,0 +1,67 @@
+/************************************************************************************/
+/* Copyright (c) 2008-2009 The Department of Arts and Culture,                      */
+/* The Government of the Republic of South Africa.                                  */
+/*                                                                                  */
+/* Contributors:  Meraka Institute, CSIR, South Africa.                             */
+/*                                                                                  */
+/* Permission is hereby granted, free of charge, to any person obtaining a copy     */
+/* of this software and associated documentation files (the "Software"), to deal    */
+/* in the Software without restriction, including without limitation the rights     */
+/* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell        */
+/* copies of the Software, and to permit persons to whom the Software is            */
+/* furnished to do so, subject to the following conditions:                         */
+/* The above copyright notice and this permission notice shall be included in       */
+/* all copies or substantial portions of the Software.                              */
+/*                                                                                  */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR       */
+/* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,         */
+/* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE      */
+/* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER           */
+/* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,    */
+/* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN        */
+/* THE SOFTWARE.                                                                    */
+/*                                                                                  */
+/************************************************************************************/
+/*                                                                                  */
+/* AUTHOR  : Aby Louw                                                               */
+/* DATE    : 25 March 2008                                                          */
+/*                                                                                  */
+/************************************************************************************/
+/*                                                                                  */
+/* The base data containers header file.                                            */
+/*                                                                                  */
+/*                                                                                  */
+/************************************************************************************/
+
+#ifndef _SPCT_BASE_CONTAINERS_INIT_H__
+#define _SPCT_BASE_CONTAINERS_INIT_H__
+
+
+/**
+ * @file src/base/containers/containers.h
+ * Base system containers includes.
+ */
+
+
+/**
+ *  @ingroup SBase
+ *  @defgroup SBaseContainers Data Containers
+ *  Provides data containers and functions.
+ */
+
+
+/************************************************************************************/
+/*                                                                                  */
+/*  Include files in base data containers                                           */
+/*                                                                                  */
+/************************************************************************************/
+
+#include "include/common.h"
+#include "base/containers/buffer/buffer.h"            /* Buffer.               */
+#include "base/containers/hashtable/hash_table.h"     /* Hash table.           */
+#include "base/containers/list/list.h"                /* Doubly linked list.   */
+
+
+
+#endif /* _SPCT_BASE_CONTAINERS_INIT_H__ */
+

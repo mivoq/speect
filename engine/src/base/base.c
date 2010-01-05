1314edba5758ba2150ea7423d638732a8d54b9f7
Aby Louw <jalouw@csir.co.za>
2010-01-05 12:44:34 +0200

    Initial commit of Speect Engine src/base header and initialization files

---
+++ engine/src/base/base.c
@@ -0,0 +1,99 @@
+/************************************************************************************/
+/* Copyright (c) 2008-2009 The Department of Arts and Culture,                      */
+/* The Government of the Republic of South Africa.                                  */
+/*                                                                                  */
+/* Contributors:  Meraka Institute, CSIR, South Africa.                             */
+/*                                                                                  */
+/* Permission is hereby granted, free of charge, to any person obtaining a copy     */
+/* of this software and associated documentation files (the "Software"), to deal    */
+/* in the Software without restriction, including without limitation the rights     */
+/* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell        */
+/* copies of the Software, and to permit persons to whom the Software is            */
+/* furnished to do so, subject to the following conditions:                         */
+/* The above copyright notice and this permission notice shall be included in       */
+/* all copies or substantial portions of the Software.                              */
+/*                                                                                  */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR       */
+/* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,         */
+/* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE      */
+/* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER           */
+/* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,    */
+/* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN        */
+/* THE SOFTWARE.                                                                    */
+/*                                                                                  */
+/************************************************************************************/
+/*                                                                                  */
+/* AUTHOR  : Aby Louw                                                               */
+/* DATE    : 25 March 2008                                                          */
+/*                                                                                  */
+/************************************************************************************/
+/*                                                                                  */
+/* The base system include file, initialization and quit.                           */
+/*                                                                                  */
+/*                                                                                  */
+/************************************************************************************/
+
+
+/************************************************************************************/
+/*                                                                                  */
+/* Modules used                                                                     */
+/*                                                                                  */
+/************************************************************************************/
+
+#include "base/base.h"
+
+
+/************************************************************************************/
+/*                                                                                  */
+/* Function implementations                                                         */
+/*                                                                                  */
+/************************************************************************************/
+
+S_LOCAL void _s_base_init(s_erc *error)
+{
+	s_erc local_err;
+
+	S_CLR_ERR(&local_err);
+	S_CLR_ERR(error);
+
+	_s_base_utils_init(error);
+	if (S_CHK_ERR(error, S_CONTERR,
+				  "_s_base_init",
+				  "Failed to intialize base utils module"))
+		local_err = *error;
+
+	_s_objsystem_init(error);
+	if (S_CHK_ERR(error, S_CONTERR,
+				  "_s_base_init",
+				  "Failed to intialize base object system module"))
+		local_err = *error;
+
+	/* if there was an error local_err will have it */
+	if ((local_err != S_SUCCESS) && (*error == S_SUCCESS))
+		*error = local_err;
+}
+
+
+S_LOCAL void _s_base_quit(s_erc *error)
+{
+	s_erc local_err;
+
+	S_CLR_ERR(&local_err);
+	S_CLR_ERR(error);
+
+	_s_objsystem_quit(error);
+	if (S_CHK_ERR(error, S_CONTERR,
+				  "_s_base_init",
+				  "Failed to quit base object system module"))
+		local_err = *error;
+
+	_s_base_utils_quit(error);
+	if (S_CHK_ERR(error, S_CONTERR,
+				  "_s_base_quit",
+				  "Failed to quit base utils module"))
+		local_err = *error;
+
+	/* if there was an error local_err will have it */
+	if ((local_err != S_SUCCESS) && (*error == S_SUCCESS))
+		*error = local_err;
+}
---
+++ engine/src/base/base.h
@@ -0,0 +1,113 @@
+/************************************************************************************/
+/* Copyright (c) 2008-2009 The Department of Arts and Culture,                      */
+/* The Government of the Republic of South Africa.                                  */
+/*                                                                                  */
+/* Contributors:  Meraka Institute, CSIR, South Africa.                             */
+/*                                                                                  */
+/* Permission is hereby granted, free of charge, to any person obtaining a copy     */
+/* of this software and associated documentation files (the "Software"), to deal    */
+/* in the Software without restriction, including without limitation the rights     */
+/* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell        */
+/* copies of the Software, and to permit persons to whom the Software is            */
+/* furnished to do so, subject to the following conditions:                         */
+/* The above copyright notice and this permission notice shall be included in       */
+/* all copies or substantial portions of the Software.                              */
+/*                                                                                  */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR       */
+/* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,         */
+/* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE      */
+/* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER           */
+/* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,    */
+/* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN        */
+/* THE SOFTWARE.                                                                    */
+/*                                                                                  */
+/************************************************************************************/
+/*                                                                                  */
+/* AUTHOR  : Aby Louw                                                               */
+/* DATE    : 25 March 2008                                                          */
+/*                                                                                  */
+/************************************************************************************/
+/*                                                                                  */
+/* The base system include file, initialization and quit.                           */
+/*                                                                                  */
+/*                                                                                  */
+/************************************************************************************/
+
+#ifndef _SPCT_BASE_H__
+#define _SPCT_BASE_H__
+
+
+/**
+ * @file base.h
+ * Base system include.
+ */
+
+
+/**
+ * @ingroup Speect
+ * @defgroup SBase Base System
+ * The non speech related core of the Speect Engine.
+ */
+
+
+/************************************************************************************/
+/*                                                                                  */
+/*  Include files in base                                                           */
+/*                                                                                  */
+/************************************************************************************/
+
+#include "include/common.h"
+#include "base/containers/containers.h" /* Containers.                      */
+#include "base/errdbg/errdbg.h"         /* Error handling and Debug module. */
+#include "base/iniparser/iniparser.h"   /* INI file parser.                 */
+#include "base/log/log.h"               /* Logging.                         */
+#include "base/objsystem/objsystem.h"   /* Object system.                   */
+#include "base/strings/utf8.h"          /* Utf8 String functions.           */
+#include "base/utils/utils.h"           /* Utilies.                         */
+
+/* threads are included through include/common.h */
+
+
+/************************************************************************************/
+/*                                                                                  */
+/* Begin external c declaration                                                     */
+/*                                                                                  */
+/************************************************************************************/
+S_BEGIN_C_DECLS
+
+
+/************************************************************************************/
+/*                                                                                  */
+/*  Function prototypes                                                             */
+/*                                                                                  */
+/************************************************************************************/
+
+/**
+ * Initialize base module.
+ * Used internally by #speect_init.
+ *
+ * @private
+ * @param error Error code.
+ */
+S_LOCAL void _s_base_init(s_erc *error);
+
+
+/**
+ * Quit base module.
+ * Used internally by #speect_quit.
+ *
+ * @private
+ * @param error Error code.
+ */
+S_LOCAL void _s_base_quit(s_erc *error);
+
+
+/************************************************************************************/
+/*                                                                                  */
+/* End external c declaration                                                       */
+/*                                                                                  */
+/************************************************************************************/
+S_END_C_DECLS
+
+
+#endif /* _SPCT_BASE_H__ */

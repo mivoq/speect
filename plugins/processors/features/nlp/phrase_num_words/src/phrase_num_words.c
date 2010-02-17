9ea1a252a863572d9d15d12bd420c4133f3444f0
Aby Louw <jalouw@csir.co.za>
2010-02-17 07:19:57 +0200

    A new feature processor. Counts the number of words in a phrase.

---
+++ plugins/processors/features/nlp/phrase_num_words/src/phrase_num_words.c
@@ -0,0 +1,182 @@
+/************************************************************************************/
+/* Copyright (c) 2010 The Department of Arts and Culture,                           */
+/* The Government of the Republic of South Africa.                                  */
+/*                                                                                  */
+/* Contributors:  Meraka Institute, CSIR, South Africa.                             */
+/*                                                                                  */
+/* Permission is hereby granted, free of charge, to any person obtaining a copy     */
+/* of this software and associated documentation files (the "Software"), to deal    */
+/* in the Software without restriction, including without limitation the rights     */
+/* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell        */
+/* copies of the Software, and to permit persons to whom the Software is            */
+/* furnished to do so, subject to the following conditions:                         */
+/* The above copyright notice and this permission notice shall be included in       */
+/* all copies or substantial portions of the Software.                              */
+/*                                                                                  */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR       */
+/* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,         */
+/* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE      */
+/* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER           */
+/* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,    */
+/* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN        */
+/* THE SOFTWARE.                                                                    */
+/*                                                                                  */
+/************************************************************************************/
+/*                                                                                  */
+/* AUTHOR  : Aby Louw                                                               */
+/* DATE    : December 2010                                                          */
+/*                                                                                  */
+/************************************************************************************/
+/*                                                                                  */
+/* Phrase num words feature processor.                                              */
+/*                                                                                  */
+/*                                                                                  */
+/************************************************************************************/
+
+
+/************************************************************************************/
+/*                                                                                  */
+/* Modules used                                                                     */
+/*                                                                                  */
+/************************************************************************************/
+
+#include "phrase_num_words.h"
+
+
+/************************************************************************************/
+/*                                                                                  */
+/* Static variables                                                                 */
+/*                                                                                  */
+/************************************************************************************/
+
+static SPhraseNumWordsFeatProcClass PhraseNumWordsFeatProcClass; /* SPhraseNumWordsFeatProc class declaration. */
+
+
+/************************************************************************************/
+/*                                                                                  */
+/* Plug-in class registration/free                                                  */
+/*                                                                                  */
+/************************************************************************************/
+
+/* local functions to register and free classes */
+S_LOCAL void _s_phrase_num_words_class_reg(s_erc *error)
+{
+	S_CLR_ERR(error);
+	s_class_reg(&PhraseNumWordsFeatProcClass, error);
+	S_CHK_ERR(error, S_CONTERR,
+			  "_s_phrase_num_words_class_reg",
+			  "Failed to register SPhraseNumWordsFeatProcClass");
+}
+
+
+S_LOCAL void _s_phrase_num_words_class_free(s_erc *error)
+{
+	S_CLR_ERR(error);
+	s_class_free(&PhraseNumWordsFeatProcClass, error);
+	S_CHK_ERR(error, S_CONTERR,
+			  "_s_phrase_num_words_class_free",
+			  "Failed to free SPhraseNumWordsFeatProcClass");
+}
+
+
+/************************************************************************************/
+/*                                                                                  */
+/* Static class function implementations                                            */
+/*                                                                                  */
+/************************************************************************************/
+
+static void Dispose(void *obj, s_erc *error)
+{
+	S_CLR_ERR(error);
+	SObjectDecRef(obj);
+}
+
+
+static SObject *Run(const SFeatProcessor *self, const SItem *item,
+					s_erc *error)
+{
+	SObject *extractedFeat = NULL;
+	const SItem *itemInPhraseRel;
+	const SItem *itrItem;
+	sint32 count;
+
+
+	S_CLR_ERR(error);
+
+	if (item == NULL)
+		return NULL;
+
+	itemInPhraseRel = SItemAs(item, "Phrase", error);
+	if (S_CHK_ERR(error, S_CONTERR,
+				  "Run",
+				  "Call to \"SItemAs\" failed"))
+		goto quit_error;
+
+	if (itemInPhraseRel == NULL)
+	{
+		extractedFeat = SObjectSetInt(0, error);
+		if (S_CHK_ERR(error, S_CONTERR,
+					  "Run",
+					  "Call to \"SObjectSetInt\" failed"))
+			goto quit_error;
+	}
+
+	itrItem = SItemDaughter(itemInPhraseRel, error);
+	if (S_CHK_ERR(error, S_CONTERR,
+				  "Run",
+				  "Call to \"SItemDaughter\" failed"))
+		goto quit_error;
+
+	count = 0;
+	while (itrItem != NULL)
+	{
+		count++;
+		itrItem = SItemNext(itrItem, error);
+		if (S_CHK_ERR(error, S_CONTERR,
+					  "Run",
+					  "Call to \"SItemNext\" failed"))
+			goto quit_error;
+	}
+
+	extractedFeat = SObjectSetInt(count, error);
+	if (S_CHK_ERR(error, S_CONTERR,
+				  "Run",
+				  "Call to \"SObjectSetInt\" failed"))
+		goto quit_error;
+
+	/* all OK here */
+	return extractedFeat;
+
+	/* error cleanup */
+quit_error:
+	if (extractedFeat != NULL)
+		S_DELETE(extractedFeat, "Run", error);
+
+	self = NULL; /* compiler noise about unused parameters */
+	return NULL;
+}
+
+
+/************************************************************************************/
+/*                                                                                  */
+/* SPhraseNumWordsFeatProc class initialization                                     */
+/*                                                                                  */
+/************************************************************************************/
+
+static SPhraseNumWordsFeatProcClass PhraseNumWordsFeatProcClass =
+{
+	/* SObjectClass */
+	{
+		"SFeatProcessor:SPhraseNumWordsFeatProc",
+		sizeof(SPhraseNumWordsFeatProc),
+		{ 0, 1},
+		NULL,            /* init    */
+		NULL,            /* destroy */
+		Dispose,         /* dispose */
+		NULL,            /* compare */
+		NULL,            /* print   */
+		NULL,            /* copy    */
+	},
+	/* SFeatProcessorClass */
+	Run                  /* run     */
+};

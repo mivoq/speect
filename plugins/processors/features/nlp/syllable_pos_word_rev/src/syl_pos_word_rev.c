75e6d5217a313bfca1dcc015bbe4f984d022e170
Aby Louw <jalouw@csir.co.za>
2010-02-17 07:25:31 +0200

    A new feature processor. Determines the position of a syllable in a word in reverse order.

---
+++ plugins/processors/features/nlp/syllable_pos_word_rev/src/syl_pos_word_rev.c
@@ -0,0 +1,216 @@
+/************************************************************************************/
+/* Copyright (c) 2010 The Department of Arts and Culture,                           */
+/* The Government of the Republic of South Africa.                                  */
+/*                                                                                  */
+/* Contributors:  Meraka Institute, CSIR, South Africa.                             */
+/*                                                                                  */
+/* Permission is hereby granted, free of charge, to any person obtaining a copy     */
+/* of this software and associated documentation files (the "Software"), to deal    */
+/* in the Software without restriction, including without limitation the rights     */
+/* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell        */
+/* copies of the Software, and to permit persons to whom the Software is            */
+/* furnished to do so, subject to the following conditions:                         */
+/* The above copyright notice and this permission notice shall be included in       */
+/* all copies or substantial portions of the Software.                              */
+/*                                                                                  */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR       */
+/* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,         */
+/* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE      */
+/* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER           */
+/* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,    */
+/* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN        */
+/* THE SOFTWARE.                                                                    */
+/*                                                                                  */
+/************************************************************************************/
+/*                                                                                  */
+/* AUTHOR  : Aby Louw                                                               */
+/* DATE    : February 2010                                                          */
+/*                                                                                  */
+/************************************************************************************/
+/*                                                                                  */
+/* Syllable position in word (reverse) feature processor.                           */
+/*                                                                                  */
+/*                                                                                  */
+/************************************************************************************/
+
+
+/************************************************************************************/
+/*                                                                                  */
+/* Modules used                                                                     */
+/*                                                                                  */
+/************************************************************************************/
+
+#include "syl_pos_word_rev.h"
+
+
+/************************************************************************************/
+/*                                                                                  */
+/* Static variables                                                                 */
+/*                                                                                  */
+/************************************************************************************/
+
+/* SSylPosWordRevFeatProc class declaration. */
+static SSylPosWordRevFeatProcClass SylPosWordRevFeatProcClass;
+
+
+/************************************************************************************/
+/*                                                                                  */
+/* Plug-in class registration/free                                                  */
+/*                                                                                  */
+/************************************************************************************/
+
+
+/* local functions to register and free classes */
+S_LOCAL void _s_syl_pos_word_rev_class_reg(s_erc *error)
+{
+	S_CLR_ERR(error);
+	s_class_reg(&SylPosWordRevFeatProcClass, error);
+	S_CHK_ERR(error, S_CONTERR,
+			  "_s_syl_pos_word_rev_class_reg",
+			  "Failed to register SSylPosWordRevFeatProcClass");
+}
+
+
+S_LOCAL void _s_syl_pos_word_rev_class_free(s_erc *error)
+{
+	S_CLR_ERR(error);
+	s_class_free(&SylPosWordRevFeatProcClass, error);
+	S_CHK_ERR(error, S_CONTERR,
+			  "_s_syl_pos_word_rev_class_free",
+			  "Failed to free SSylPosWordRevFeatProcClass");
+}
+
+
+/************************************************************************************/
+/*                                                                                  */
+/* Static class function implementations                                            */
+/*                                                                                  */
+/************************************************************************************/
+
+static void Dispose(void *obj, s_erc *error)
+{
+	S_CLR_ERR(error);
+	SObjectDecRef(obj);
+}
+
+
+static SObject *Run(const SFeatProcessor *self, const SItem *item,
+					s_erc *error)
+{
+	SObject *extractedFeat = NULL;
+	const SItem *itemInSylStructRel;
+	const SItem *wordItem;
+	const SItem *itr;
+	sint32 count;
+
+
+	S_CLR_ERR(error);
+
+	if (item == NULL)
+		return NULL;
+
+	itemInSylStructRel = SItemAs(item, "SylStructure", error);
+	if (S_CHK_ERR(error, S_CONTERR,
+				  "Run",
+				  "Call to \"SItemAs\" failed"))
+		goto quit_error;
+
+	if (itemInSylStructRel == NULL)
+	{
+		extractedFeat = SObjectSetInt(0, error);
+		if (S_CHK_ERR(error, S_CONTERR,
+					  "Run",
+					  "Call to \"SObjectSetInt\" failed"))
+			goto quit_error;
+
+		return extractedFeat;
+	}
+
+	wordItem = SItemParent(itemInSylStructRel, error);
+	if (S_CHK_ERR(error, S_CONTERR,
+				  "Run",
+				  "Call to \"SItemParent\" failed"))
+		goto quit_error;
+
+	if (wordItem == NULL)
+	{
+		extractedFeat = SObjectSetInt(0, error);
+		if (S_CHK_ERR(error, S_CONTERR,
+					  "Run",
+					  "Call to \"SObjectSetInt\" failed"))
+			goto quit_error;
+
+		return extractedFeat;
+	}
+
+	itr = SItemLastDaughter(wordItem, error);
+	if (S_CHK_ERR(error, S_CONTERR,
+				  "Run",
+				  "Call to \"SItemLastDaughter\" failed"))
+		goto quit_error;
+
+	count = 0;
+	while (itr != NULL)
+	{
+		s_bool is_equal;
+
+
+		is_equal = SItemEqual(itr, itemInSylStructRel, error);
+		if (S_CHK_ERR(error, S_CONTERR,
+					  "Run",
+					  "Call to \"SItemEqual\" failed"))
+			goto quit_error;
+
+		if (is_equal)
+			break;
+
+		count++;
+		itr = SItemPrev(itr, error);
+		if (S_CHK_ERR(error, S_CONTERR,
+					  "Run",
+					  "Call to \"SItemPrev\" failed"))
+			goto quit_error;
+	}
+
+	extractedFeat = SObjectSetInt(count, error);
+	if (S_CHK_ERR(error, S_CONTERR,
+				  "Run",
+				  "Call to \"SObjectSetInt\" failed"))
+		goto quit_error;
+
+	/* all OK here */
+	return extractedFeat;
+
+	/* error cleanup */
+quit_error:
+	if (extractedFeat != NULL)
+		S_DELETE(extractedFeat, "Run", error);
+
+	self = NULL; /* compiler noise about unused parameters */
+	return NULL;
+}
+
+
+/************************************************************************************/
+/*                                                                                  */
+/* SSylPosWordRevFeatProc class initialization                                      */
+/*                                                                                  */
+/************************************************************************************/
+
+static SSylPosWordRevFeatProcClass SylPosWordRevFeatProcClass =
+{
+	/* SObjectClass */
+	{
+		"SFeatProcessor:SSylPosWordRevFeatProc",
+		sizeof(SSylPosWordRevFeatProc),
+		{ 0, 1},
+		NULL,            /* init    */
+		NULL,            /* destroy */
+		Dispose,         /* dispose */
+		NULL,            /* compare */
+		NULL,            /* print   */
+		NULL,            /* copy    */
+	},
+	/* SFeatProcessorClass */
+	Run                  /* run     */
+};

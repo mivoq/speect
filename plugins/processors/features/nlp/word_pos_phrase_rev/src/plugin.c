fd0dde14f71056cdea3fe91a391432bffef753a0
Aby Louw <jalouw@csir.co.za>
2010-02-17 07:26:49 +0200

    A new feature processor. Determines the position of a word in a phrase in reverse order.

---
+++ plugins/processors/features/nlp/word_pos_phrase_rev/src/plugin.c
@@ -0,0 +1,158 @@
+/************************************************************************************/
+/* Copyright (c) 2010 The Department of Arts and Culture,                           */
+/* The Government of the Republic of South Africa.                                  */
+/*                                                                                  */
+/* Contributors:  Meraka Institute, CSIR, South Africa.                             */
+/*                                                                                  */
+/* Permission is hereby granted, free of charge, to any person obtaining a copy     */
+/* of this software and associated documentation files (the "Software"), to deal    */
+/* in the Software without restriction, including without limitation the rights     */
+/* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell        */
+/* copies of the Software, and to permit persons to whom the Software is            */
+/* furnished to do so, subject to the following conditions:                         */
+/* The above copyright notice and this permission notice shall be included in       */
+/* all copies or substantial portions of the Software.                              */
+/*                                                                                  */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR       */
+/* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,         */
+/* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE      */
+/* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER           */
+/* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,    */
+/* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN        */
+/* THE SOFTWARE.                                                                    */
+/*                                                                                  */
+/************************************************************************************/
+/*                                                                                  */
+/* AUTHOR  : Aby Louw                                                               */
+/* DATE    : February 2010                                                          */
+/*                                                                                  */
+/************************************************************************************/
+/*                                                                                  */
+/* Word position in phrase (reverse) feature processor.                             */
+/*                                                                                  */
+/*                                                                                  */
+/************************************************************************************/
+
+
+/************************************************************************************/
+/*                                                                                  */
+/* Modules used                                                                     */
+/*                                                                                  */
+/************************************************************************************/
+
+#include "word_pos_phrase_rev.h"
+
+
+/************************************************************************************/
+/*                                                                                  */
+/* Static variables                                                                 */
+/*                                                                                  */
+/************************************************************************************/
+
+static const char * const plugin_init_func = "SWordPosPhraseRevFeatProc plug-in initialization";
+
+static const char * const plugin_exit_func = "SWordPosPhraseRevFeatProc plug-in free";
+
+
+/************************************************************************************/
+/*                                                                                  */
+/* Static function prototypes                                                       */
+/*                                                                                  */
+/************************************************************************************/
+
+static s_bool version_ok(const s_lib_version version);
+
+static void plugin_exit_function(s_erc *error);
+
+
+/************************************************************************************/
+/*                                                                                  */
+/* Plug-in parameters                                                               */
+/*                                                                                  */
+/************************************************************************************/
+
+static const s_plugin_params plugin_params =
+{
+	/* plug-in name */
+	"Syllable position in word (reverse) feature processor",
+
+	/* description */
+	"The position (reverse) of the given word in it's phrase.",
+
+	/* version */
+	{
+		0,
+		1
+	},
+
+	/* Speect ABI version (which plug-in was compiled with) */
+	{
+		0,
+		9
+	},
+
+	/* exit function pointer */
+	plugin_exit_function
+};
+
+
+/************************************************************************************/
+/*                                                                                  */
+/* Function implementations                                                         */
+/*                                                                                  */
+/************************************************************************************/
+
+const s_plugin_params *s_plugin_init(const s_lib_version version, s_erc *error)
+{
+	S_CLR_ERR(error);
+
+	if (!version_ok(version))
+	{
+		S_CTX_ERR(error, S_FAILURE,
+				  plugin_init_func,
+				  "Incorrect Speect Engine version, require '0.9.x'");
+		return NULL;
+	}
+
+	/* register plug-in classes here */
+	_s_word_pos_phrase_rev_class_reg(error);
+	if (S_CHK_ERR(error, S_CONTERR,
+				  plugin_init_func,
+				  "Failed to register SWordPosPhraseRevFeatProc class"))
+		return NULL;
+
+	return &plugin_params;
+}
+
+
+/************************************************************************************/
+/*                                                                                  */
+/* Static function implementations                                                  */
+/*                                                                                  */
+/************************************************************************************/
+
+/* check the Speect Engine version */
+static s_bool version_ok(const s_lib_version version)
+{
+	/*
+	 * we want Speect Engine 0.9.x
+	 */
+	if ((version.major == 0)
+		&& (version.minor == 9))
+		return TRUE;
+
+	return FALSE;
+}
+
+
+/* plug-in exit function */
+static void plugin_exit_function(s_erc *error)
+{
+	S_CLR_ERR(error);
+
+	/* free plug-in classes here */
+	_s_word_pos_phrase_rev_class_free(error);
+	S_CHK_ERR(error, S_CONTERR,
+			  plugin_exit_func,
+			  "Failed to free SWordPosPhraseRevFeatProc class");
+}
---
+++ plugins/processors/features/nlp/word_pos_phrase_rev/src/word_pos_phrase_rev.c
@@ -0,0 +1,214 @@
+/************************************************************************************/
+/* Copyright (c) 2010 The Department of Arts and Culture,                           */
+/* The Government of the Republic of South Africa.                                  */
+/*                                                                                  */
+/* Contributors:  Meraka Institute, CSIR, South Africa.                             */
+/*                                                                                  */
+/* Permission is hereby granted, free of charge, to any person obtaining a copy     */
+/* of this software and associated documentation files (the "Software"), to deal    */
+/* in the Software without restriction, including without limitation the rights     */
+/* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell        */
+/* copies of the Software, and to permit persons to whom the Software is            */
+/* furnished to do so, subject to the following conditions:                         */
+/* The above copyright notice and this permission notice shall be included in       */
+/* all copies or substantial portions of the Software.                              */
+/*                                                                                  */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR       */
+/* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,         */
+/* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE      */
+/* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER           */
+/* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,    */
+/* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN        */
+/* THE SOFTWARE.                                                                    */
+/*                                                                                  */
+/************************************************************************************/
+/*                                                                                  */
+/* AUTHOR  : Aby Louw                                                               */
+/* DATE    : February 2010                                                          */
+/*                                                                                  */
+/************************************************************************************/
+/*                                                                                  */
+/* Word position in phrase (reverse) feature processor.                             */
+/*                                                                                  */
+/*                                                                                  */
+/************************************************************************************/
+
+
+/************************************************************************************/
+/*                                                                                  */
+/* Modules used                                                                     */
+/*                                                                                  */
+/************************************************************************************/
+
+#include "word_pos_phrase_rev.h"
+
+
+/************************************************************************************/
+/*                                                                                  */
+/* Static variables                                                                 */
+/*                                                                                  */
+/************************************************************************************/
+
+static SWordPosPhraseRevFeatProcClass WordPosPhraseRevFeatProcClass; /* SWordPosPhraseRevFeatProc class declaration. */
+
+
+/************************************************************************************/
+/*                                                                                  */
+/* Plug-in class registration/free                                                  */
+/*                                                                                  */
+/************************************************************************************/
+
+/* local functions to register and free classes */
+S_LOCAL void _s_word_pos_phrase_rev_class_reg(s_erc *error)
+{
+	S_CLR_ERR(error);
+	s_class_reg(&WordPosPhraseRevFeatProcClass, error);
+	S_CHK_ERR(error, S_CONTERR,
+			  "_s_word_pos_phrase_rev_class_reg",
+			  "Failed to register SWordPosPhraseRevFeatProcClass");
+}
+
+
+S_LOCAL void _s_word_pos_phrase_rev_class_free(s_erc *error)
+{
+	S_CLR_ERR(error);
+	s_class_free(&WordPosPhraseRevFeatProcClass, error);
+	S_CHK_ERR(error, S_CONTERR,
+			  "_s_word_pos_phrase_rev_class_free",
+			  "Failed to free SWordPosPhraseRevFeatProcClass");
+}
+
+
+/************************************************************************************/
+/*                                                                                  */
+/* Static class function implementations                                            */
+/*                                                                                  */
+/************************************************************************************/
+
+static void Dispose(void *obj, s_erc *error)
+{
+	S_CLR_ERR(error);
+	SObjectDecRef(obj);
+}
+
+
+static SObject *Run(const SFeatProcessor *self, const SItem *item,
+					s_erc *error)
+{
+	SObject *extractedFeat = NULL;
+	const SItem *itemInPhraseRel;
+	const SItem *phraseItem;
+	const SItem *itr;
+	sint32 count;
+
+
+	S_CLR_ERR(error);
+
+	if (item == NULL)
+		return NULL;
+
+	itemInPhraseRel = SItemAs(item, "Phrase", error);
+	if (S_CHK_ERR(error, S_CONTERR,
+				  "Run",
+				  "Call to \"SItemAs\" failed"))
+		goto quit_error;
+
+	if (itemInPhraseRel == NULL)
+	{
+		extractedFeat = SObjectSetInt(0, error);
+		if (S_CHK_ERR(error, S_CONTERR,
+					  "Run",
+					  "Call to \"SObjectSetInt\" failed"))
+			goto quit_error;
+
+		return extractedFeat;
+	}
+
+	phraseItem = SItemParent(itemInPhraseRel, error);
+	if (S_CHK_ERR(error, S_CONTERR,
+				  "Run",
+				  "Call to \"SItemParent\" failed"))
+		goto quit_error;
+
+	if (phraseItem == NULL)
+	{
+		extractedFeat = SObjectSetInt(0, error);
+		if (S_CHK_ERR(error, S_CONTERR,
+					  "Run",
+					  "Call to \"SObjectSetInt\" failed"))
+			goto quit_error;
+
+		return extractedFeat;
+	}
+
+	itr = SItemLastDaughter(phraseItem, error);
+	if (S_CHK_ERR(error, S_CONTERR,
+				  "Run",
+				  "Call to \"SItemLastDaughter\" failed"))
+		goto quit_error;
+
+	count = 0;
+	while (itr != NULL)
+	{
+		s_bool is_equal;
+
+
+		is_equal = SItemEqual(itr, itemInPhraseRel, error);
+		if (S_CHK_ERR(error, S_CONTERR,
+					  "Run",
+					  "Call to \"SItemEqual\" failed"))
+			goto quit_error;
+
+		if (is_equal)
+			break;
+
+		count++;
+		itr = SItemPrev(itr, error);
+		if (S_CHK_ERR(error, S_CONTERR,
+					  "Run",
+					  "Call to \"SItemPrev\" failed"))
+			goto quit_error;
+	}
+
+	extractedFeat = SObjectSetInt(count, error);
+	if (S_CHK_ERR(error, S_CONTERR,
+				  "Run",
+				  "Call to \"SObjectSetInt\" failed"))
+		goto quit_error;
+
+	/* all OK here */
+	return extractedFeat;
+
+	/* error cleanup */
+quit_error:
+	if (extractedFeat != NULL)
+		S_DELETE(extractedFeat, "Run", error);
+
+	self = NULL; /* compiler noise about unused parameters */
+	return NULL;
+}
+
+
+/************************************************************************************/
+/*                                                                                  */
+/* SWordPosPhraseRevFeatProc class initialization                                   */
+/*                                                                                  */
+/************************************************************************************/
+
+static SWordPosPhraseRevFeatProcClass WordPosPhraseRevFeatProcClass =
+{
+	/* SObjectClass */
+	{
+		"SFeatProcessor:SWordPosPhraseRevFeatProc",
+		sizeof(SWordPosPhraseRevFeatProc),
+		{ 0, 1},
+		NULL,            /* init    */
+		NULL,            /* destroy */
+		Dispose,         /* dispose */
+		NULL,            /* compare */
+		NULL,            /* print   */
+		NULL,            /* copy    */
+	},
+	/* SFeatProcessorClass */
+	Run                  /* run     */
+};

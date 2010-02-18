574e5edc7ea0e411bd3cf00903b29885cb4ac7a9
Aby Louw <jalouw@csir.co.za>
2010-02-18 13:29:30 +0200

    A new utterance processor, SSylToneUttProc It's a "post-lex" processor and only works for lexicon or addendum entries that specify the syllables of the word. Also the last "phone" of the syllable must be a tone marker that will become a feature of the syllable relation items. This is for Mpho for testing her stuff.

---
+++ plugins/processors/utterances/nlp/sylltone/src/plugin.c
@@ -0,0 +1,159 @@
+/************************************************************************************/
+/* Copyright (c) 2010 The Department of Arts and Culture,                           */
+/* The Government of the Republic of South Africa.                                  */
+/*                                                                                  */
+/* Contributors:  Meraka Institute, CSIR, South Africa.                             */
+/*                                                                                  */
+/* Permission is hereby granted, free of charge, to any person obtaining a copy     */
+/* of this software and associated documentation files (the "Software"), to deal    */
+/* in the Software without restriction, including without limitation the rights     */
+/* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell        */
+/* copies of the Software, and to permit persons to whom the Software is            */
+/* furnished to do so, subject to the following conditions:                         */
+/* The above copyright notice and this permission notice shall be included in       */
+/* all copies or substantial portions of the Software.                              */
+/*                                                                                  */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR       */
+/* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,         */
+/* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE      */
+/* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER           */
+/* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,    */
+/* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN        */
+/* THE SOFTWARE.                                                                    */
+/*                                                                                  */
+/************************************************************************************/
+/*                                                                                  */
+/* AUTHOR  : Aby Louw                                                               */
+/* DATE    : February 2010                                                          */
+/*                                                                                  */
+/************************************************************************************/
+/*                                                                                  */
+/* A syllable tone, post lex, utterance processor.                                  */
+/*                                                                                  */
+/*                                                                                  */
+/************************************************************************************/
+
+
+/************************************************************************************/
+/*                                                                                  */
+/* Modules used                                                                     */
+/*                                                                                  */
+/************************************************************************************/
+
+#include "sylltone.h"
+
+
+/************************************************************************************/
+/*                                                                                  */
+/* Static variables                                                                 */
+/*                                                                                  */
+/************************************************************************************/
+
+static const char * const plugin_init_func = "SSylToneUttProc plug-in initialization";
+
+static const char * const plugin_exit_func = "SSylToneUttProc plug-in free";
+
+
+/************************************************************************************/
+/*                                                                                  */
+/* Static function prototypes                                                       */
+/*                                                                                  */
+/************************************************************************************/
+
+static s_bool version_ok(const s_lib_version version);
+
+static void plugin_exit_function(s_erc *error);
+
+
+/************************************************************************************/
+/*                                                                                  */
+/* Plug-in parameters                                                               */
+/*                                                                                  */
+/************************************************************************************/
+
+static const s_plugin_params plugin_params =
+{
+	/* plug-in name */
+	"A Syllable tone, post lex, utterance processor",
+
+	/* description */
+	"An utterance processor that gets a syllable's tone from the last \"phone\" in "
+	"the syllable, which is actually a tone specification.",
+
+	/* version */
+	{
+		0,
+		1
+	},
+
+	/* Speect ABI version (which plug-in was compiled with) */
+	{
+		0,
+		9
+	},
+
+	/* exit function pointer */
+	plugin_exit_function
+};
+
+
+/************************************************************************************/
+/*                                                                                  */
+/* Function implementations                                                         */
+/*                                                                                  */
+/************************************************************************************/
+
+const s_plugin_params *s_plugin_init(const s_lib_version version, s_erc *error)
+{
+	S_CLR_ERR(error);
+
+	if (!version_ok(version))
+	{
+		S_CTX_ERR(error, S_FAILURE,
+				  plugin_init_func,
+				  "Incorrect Speect Engine version, require '0.9.x'");
+		return NULL;
+	}
+
+	/* register plug-in classes here */
+	_s_sylltone_utt_proc_class_reg(error);
+	if (S_CHK_ERR(error, S_CONTERR,
+				  plugin_init_func,
+				  "Failed to register SSylToneUttProc class"))
+		return NULL;
+
+	return &plugin_params;
+}
+
+
+/************************************************************************************/
+/*                                                                                  */
+/* Static function implementations                                                  */
+/*                                                                                  */
+/************************************************************************************/
+
+/* check the Speect Engine version */
+static s_bool version_ok(const s_lib_version version)
+{
+	/*
+	 * we want Speect Engine 0.9.x
+	 */
+	if ((version.major == 0)
+		&& (version.minor == 9))
+		return TRUE;
+
+	return FALSE;
+}
+
+
+/* plug-in exit function */
+static void plugin_exit_function(s_erc *error)
+{
+	S_CLR_ERR(error);
+
+	/* free plug-in classes here */
+	_s_sylltone_utt_proc_class_free(error);
+	S_CHK_ERR(error, S_CONTERR,
+			  plugin_exit_func,
+			  "Failed to free SSylToneUttProc class");
+}
---
+++ plugins/processors/utterances/nlp/sylltone/src/sylltone.c
@@ -0,0 +1,202 @@
+/************************************************************************************/
+/* Copyright (c) 2010 The Department of Arts and Culture,                           */
+/* The Government of the Republic of South Africa.                                  */
+/*                                                                                  */
+/* Contributors:  Meraka Institute, CSIR, South Africa.                             */
+/*                                                                                  */
+/* Permission is hereby granted, free of charge, to any person obtaining a copy     */
+/* of this software and associated documentation files (the "Software"), to deal    */
+/* in the Software without restriction, including without limitation the rights     */
+/* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell        */
+/* copies of the Software, and to permit persons to whom the Software is            */
+/* furnished to do so, subject to the following conditions:                         */
+/* The above copyright notice and this permission notice shall be included in       */
+/* all copies or substantial portions of the Software.                              */
+/*                                                                                  */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR       */
+/* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,         */
+/* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE      */
+/* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER           */
+/* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,    */
+/* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN        */
+/* THE SOFTWARE.                                                                    */
+/*                                                                                  */
+/************************************************************************************/
+/*                                                                                  */
+/* AUTHOR  : Aby Louw                                                               */
+/* DATE    : February 2010                                                          */
+/*                                                                                  */
+/************************************************************************************/
+/*                                                                                  */
+/* A syllable tone, post lex, utterance processor.                                  */
+/*                                                                                  */
+/*                                                                                  */
+/************************************************************************************/
+
+
+/************************************************************************************/
+/*                                                                                  */
+/* Modules used                                                                     */
+/*                                                                                  */
+/************************************************************************************/
+
+#include "sylltone.h"
+
+
+/************************************************************************************/
+/*                                                                                  */
+/* Static variables                                                                 */
+/*                                                                                  */
+/************************************************************************************/
+
+static SSylToneUttProcClass SylToneUttProcClass; /* SSylToneUttProcClass class declaration. */
+
+
+/************************************************************************************/
+/*                                                                                  */
+/* Plug-in class registration/free                                                  */
+/*                                                                                  */
+/************************************************************************************/
+
+/* local functions to register and free classes */
+S_LOCAL void _s_sylltone_utt_proc_class_reg(s_erc *error)
+{
+	S_CLR_ERR(error);
+	s_class_reg(&SylToneUttProcClass, error);
+	S_CHK_ERR(error, S_CONTERR,
+			  "_s_sylltone_utt_proc_class_reg",
+			  "Failed to register SSylToneUttProcClass");
+}
+
+
+S_LOCAL void _s_sylltone_utt_proc_class_free(s_erc *error)
+{
+	S_CLR_ERR(error);
+	s_class_free(&SylToneUttProcClass, error);
+	S_CHK_ERR(error, S_CONTERR,
+			  "_s_sylltone_utt_proc_class_free",
+			  "Failed to free SSylToneUttProcClass");
+}
+
+
+/************************************************************************************/
+/*                                                                                  */
+/* Static class function implementations                                            */
+/*                                                                                  */
+/************************************************************************************/
+
+static void Dispose(void *obj, s_erc *error)
+{
+	S_CLR_ERR(error);
+	SObjectDecRef(obj);
+}
+
+
+static void Run(const SUttProcessor *self, SUtterance *utt,
+				s_erc *error)
+{
+	const SRelation *sylRel;
+	const SItem *sylItr;
+	const SItem *sylAsSylStruct;
+	const SItem *tone;
+	const SItem *toneAsSegment;
+	s_bool is_present;
+
+
+	S_CLR_ERR(error);
+
+	/* we require the Syllable relation */
+	is_present = SUtteranceRelationIsPresent(utt, "Syllable", error);
+	if (S_CHK_ERR(error, S_CONTERR,
+				  "Run",
+				  "Call to \"SUtteranceRelationIsPresent\" failed"))
+		goto quit_error;
+
+	if (!is_present)
+	{
+		S_CTX_ERR(error, S_FAILURE,
+				  "Run",
+				  "Failed to find 'Syllable' relation in utterance");
+		goto quit_error;
+	}
+
+	sylRel = SUtteranceGetRelation(utt, "Syllable", error);
+	if (S_CHK_ERR(error, S_CONTERR,
+				  "Run",
+				  "Call to \"SUtteranceGetRelation\" failed"))
+		goto quit_error;
+
+	sylItr = SRelationHead(sylRel, error);
+	if (S_CHK_ERR(error, S_CONTERR,
+				  "Run",
+				  "Call to \"SRelationHead\" failed"))
+		goto quit_error;
+
+	while (sylItr != NULL)
+	{
+		sylAsSylStruct = SItemAs(sylItr, "SylStructure", error);
+		if (S_CHK_ERR(error, S_CONTERR,
+					  "Run",
+					  "Call to \"SItemAs\" failed"))
+			goto quit_error;
+
+		tone = SItemLastDaughter(sylAsSylStruct, error);
+		if (S_CHK_ERR(error, S_CONTERR,
+					  "Run",
+					  "Call to \"SItemLastDaughter\" failed"))
+			goto quit_error;
+
+		SItemSetObject((SItem*)sylAsSylStruct, "tone",
+					   SItemGetObject(tone, "name", error), error);
+		if (S_CHK_ERR(error, S_CONTERR,
+					  "Run",
+					  "Call to \"SItemSetObject/SItemGetObject\" failed"))
+			goto quit_error;
+
+		toneAsSegment = SItemAs(tone, "Segment", error);
+
+		S_FORCE_DELETE(tone, "Run", error);
+		S_FORCE_DELETE(toneAsSegment, "Run", error);
+
+		sylItr = SItemNext(sylItr, error);
+		if (S_CHK_ERR(error, S_CONTERR,
+					  "Run",
+					  "Call to \"SItemNext\" failed"))
+			goto quit_error;
+	}
+
+
+	/* here all is OK */
+	return;
+
+
+	/* error clean-up code */
+quit_error:
+	self = NULL;
+}
+
+
+/************************************************************************************/
+/*                                                                                  */
+/* STokenizationUttProc class initialization                                        */
+/*                                                                                  */
+/************************************************************************************/
+
+static SSylToneUttProcClass SylToneUttProcClass =
+{
+	/* SObjectClass */
+	{
+		"SUttProcessor:SSylToneUttProc",
+		sizeof(SSylToneUttProc),
+		{ 0, 1},
+		NULL,            /* init    */
+		NULL,            /* destroy */
+		Dispose,         /* dispose */
+		NULL,            /* compare */
+		NULL,            /* print   */
+		NULL,            /* copy    */
+	},
+	/* SUttProcessorClass */
+	NULL,                /* initialize */
+	Run                  /* run        */
+};

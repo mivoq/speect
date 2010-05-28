96b53f0a9c4ff4d664794d04fc4e7148fbc221e0
Aby Louw <jalouw@csir.co.za>
2010-05-28 04:15:23 +0200

    WIN32 file system path functions

---
+++ engine/src/base/utils/platform/win32/win32_path.h
@@ -0,0 +1,102 @@
+/************************************************************************************/
+/* Copyright (c) 2010 The Department of Arts and Culture,                           */
+/* The Government of the Republic of South Africa.                                  */
+/*                                                                                  */
+/* Contributors:  Meraka Institute, CSIR, South Africa.                             */
+/*                                                                                  */
+/* Permission is hereby granted, free of charge, to any person obtaining a copy     */
+/* of this software and associated documentation files (the "Software"), to deal    */
+/* in the Software without restriction, including without limitation the rights     */
+/* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell        */
+/* copies of the Software, and to permit persons to whom the Software is            */
+/* furnished to do so, subject to the following conditions:                         */
+/* The above copyright notice and this permission notice shall be included in       */
+/* all copies or substantial portions of the Software.                              */
+/*                                                                                  */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR       */
+/* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,         */
+/* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE      */
+/* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER           */
+/* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,    */
+/* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN        */
+/* THE SOFTWARE.                                                                    */
+/*                                                                                  */
+/************************************************************************************/
+/*                                                                                  */
+/* AUTHOR  : Aby Louw                                                               */
+/* DATE    : May 2010                                                               */
+/*                                                                                  */
+/************************************************************************************/
+/*                                                                                  */
+/* Windows file system paths.                                                       */
+/*                                                                                  */
+/*                                                                                  */
+/************************************************************************************/
+
+#ifndef _SPCT_WIN32_PATH_H__
+#define _SPCT_WIN32_PATH_H__
+
+
+/**
+ * @file win32_path.h
+ * Windows file system paths.
+ */
+
+
+/************************************************************************************/
+/*                                                                                  */
+/* Modules used                                                                     */
+/*                                                                                  */
+/************************************************************************************/
+
+#include "include/common.h"
+#include "base/errdbg/errdbg.h"
+
+
+/************************************************************************************/
+/*                                                                                  */
+/* Begin external c declaration                                                     */
+/*                                                                                  */
+/************************************************************************************/
+S_BEGIN_C_DECLS
+
+
+/************************************************************************************/
+/*                                                                                  */
+/* Macros                                                                           */
+/*                                                                                  */
+/************************************************************************************/
+
+/* defines of the WIN32 path functions wrappers */
+
+#define _S_PATH_COMBINE(BASEPATH, FILENAME, ERROR)	\
+	s_win32_path_combine(BASEPATH, FILENAME, ERROR)
+
+
+#define _S_GET_BASE_PATH(ABSOLUTE_FILENAME, ERROR)	\
+	s_win32_get_base_path(ABSOLUTE_FILENAME, ERROR)
+
+
+/************************************************************************************/
+/*                                                                                  */
+/* Function prototypes                                                              */
+/*                                                                                  */
+/************************************************************************************/
+
+S_LOCAL char *s_win32_path_combine(const char *base_path, const char *filename,
+								   s_erc *error);
+
+
+S_LOCAL char *s_win32_get_base_path(const char *absolute_filename, s_erc *error);
+
+
+/***********************************************************************************/
+/*                                                                                 */
+/* End external c declaration                                                      */
+/*                                                                                 */
+/***********************************************************************************/
+S_END_C_DECLS
+
+
+#endif /* _SPCT_WIN32_PATH_H__ */
+

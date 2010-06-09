94d66f7cb2501efdf1b6a9891e700994d9b0a138
Aby Louw <jalouw@csir.co.za>
2010-06-09 14:46:30 +0200

    Fixed CMake variables
      * speect_conf_install.cmake is now generated in CMAKE_BINARY_DIR, from
        engineCMakeConf.cmake
    
      * speect_conf_install.cmake installation of SpeectConf.cmake now works
        and correctly adds SpeectConf.cmake to install_manifest.txt

--- engine/cmake/engineCMakeConf.cmake
+++ engine/cmake/engineCMakeConf.cmake
@@ -15,9 +15,9 @@ configure_file(${CMAKE_SOURCE_DIR}/engine/config/SpeectConf.cmake.in
     ${CMAKE_BINARY_DIR}/engine/SpeectConf.cmake @ONLY)
 
 configure_file(${CMAKE_SOURCE_DIR}/engine/config/speect_conf_install.cmake.in 
-  ${CMAKE_SOURCE_DIR}/engine/cmake/speect_conf_install.cmake @ONLY)
+  ${CMAKE_BINARY_DIR}/engine/cmake/speect_conf_install.cmake @ONLY)
 
 export(TARGETS SPCT_LIBRARIES
   FILE spctConfig.cmake)
 
-install(SCRIPT ${CMAKE_SOURCE_DIR}/engine/cmake/speect_conf_install.cmake)
+install(SCRIPT ${CMAKE_BINARY_DIR}/engine/cmake/speect_conf_install.cmake)

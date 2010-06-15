ac8cb65f0aa44b5bd1f19bd7c4895c6324e12ccf
Aby Louw <jalouw@csir.co.za>
2010-06-15 02:50:34 +0200

    Refactoring and updating to Speect Engine 1.0.0
        * Removed unused directories/files
        * Updated build system functions
        * Updated s_plugin_params structure
        * Plug-in info is now autogenerated.

--- plugins/utils/cost_function/cmake/sources.cmake
+++ plugins/utils/cost_function/cmake/sources.cmake
@@ -1,11 +1,11 @@
 ######################################################################################
 ##                                                                                  ##
 ## AUTHOR  : Aby Louw                                                               ##
-## DATE    : 5 November 2009                                                        ##
+## DATE    : June 2010                                                              ##
 ##                                                                                  ##
 ######################################################################################
 ##                                                                                  ##
-## Template CMake source files for Speect plug-ins                                  ##
+## Source files for Cost Functions plug-in                                          ##
 ##                                                                                  ##
 ##                                                                                  ##
 ######################################################################################
@@ -13,7 +13,7 @@
 
 ######## source files ##################
 
-set(${PLUGIN_SONAME}_SRC_FILES
+speect_plugin_sources(
   src/plugin.c
   src/cost_function.c
   )
@@ -21,7 +21,7 @@ set(${PLUGIN_SONAME}_SRC_FILES
 
 ######## header files ##################
 
-set(${PLUGIN_SONAME}_INCLUDE_FILES
+speect_plugin_headers(
   src/cost_function.h
   )
 

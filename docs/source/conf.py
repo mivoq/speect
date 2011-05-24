146c367ef4b65fb1a2d437fad49b9b213f24db5a
Aby Louw <jalouw@csir.co.za>
2011-05-24 06:19:28 +0200

    Added sphinx.ext.graphviz so that we can include inheritance diagrams.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,12 +30,9 @@ extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.todo',
               'sphinx.ext.pngmath',
               'sphinx.ext.ifconfig',
+              'sphinx.ext.graphviz',
               'doxybridge',
               ]
-
-# The output format for Graphviz when building HTML files.
-# This must be either 'png' or 'svg'; the default is 'png'.
-graphviz_output_format = 'png'
 
 # include todo's
 todo_include_todos = True

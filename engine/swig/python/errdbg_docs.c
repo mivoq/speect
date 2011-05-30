f2953bd9eeea41b03fd4e73dd7d849b8acb2ea28
Aby Louw <jalouw@csir.co.za>
2011-05-30 20:23:11 +0200

    Added s_errdbg_on as Python function and docs.

---
+++ engine/swig/python/errdbg_docs.c
@@ -0,0 +1,50 @@
+/************************************************************************************/
+/* Copyright (c) 2011 The Department of Arts and Culture,                           */
+/* The Government of the Republic of South Africa.                                  */
+/*                                                                                  */
+/* Contributors:  Meraka Institute, CSIR, South Africa.                             */
+/*                                                                                  */
+/* Permission is hereby granted, free of charge, to any person obtaining a copy     */
+/* of this software and associated documentation files (the "Software"), to deal    */
+/* in the Software without restriction, including without limitation the rights     */
+/* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell        */
+/* copies of the Software, and to permit persons to whom the Software is            */
+/* furnished to do so, subject to the following conditions:                         */
+/* The above copyright notice and this permission notice shall be included in       */
+/* all copies or substantial portions of the Software.                              */
+/*                                                                                  */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR       */
+/* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,         */
+/* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE      */
+/* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER           */
+/* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,    */
+/* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN        */
+/* THE SOFTWARE.                                                                    */
+/*                                                                                  */
+/************************************************************************************/
+/*                                                                                  */
+/* AUTHOR  : Aby Louw                                                               */
+/* DATE    : May 2011                                                               */
+/*                                                                                  */
+/************************************************************************************/
+/*                                                                                  */
+/* Python documentation strings for Error and Debugging Speect functions.           */
+/*                                                                                  */
+/*                                                                                  */
+/************************************************************************************/
+
+
+%define errdbg_is_on_DOCSTRING
+"""
+Query whether the *Error and Debugging System* is activated or
+not. The system can be activated/deactivate with the compile time
+definition of ``SPCT_ERROR_HANDLING``. If deactivated then no
+logging or checking of error codes will occur.
+
+:rtype: bool
+"""
+%enddef
+
+%feature("autodoc", errdbg_is_on_DOCSTRING) errdbg_is_on;
+
+

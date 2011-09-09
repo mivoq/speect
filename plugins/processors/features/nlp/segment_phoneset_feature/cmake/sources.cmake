0e78e20fe807223f14fe7de248e826259603ad6f
Aby Louw <jalouw@csir.co.za>
2011-09-09 12:29:16 +0200

    Added segment_phoneset_feature feature processor. Can be used to access any phoneset feature as a path by just prepending "ph_" before the phoneset feature name.

---
+++ plugins/processors/features/nlp/segment_phoneset_feature/cmake/sources.cmake
@@ -0,0 +1,27 @@
+######################################################################################
+##                                                                                  ##
+## AUTHOR  : Aby Louw                                                               ##
+## DATE    : September 2011                                                         ##
+##                                                                                  ##
+######################################################################################
+##                                                                                  ##
+## Source files for Segment phoneset feature, feature processor plug-in             ##
+##                                                                                  ##
+##                                                                                  ##
+######################################################################################
+
+
+######## source files ##################
+
+speect_plugin_sources(
+  src/plugin.c
+  src/seg_ph_feat.c
+  )
+ 
+
+######## header files ##################
+
+speect_plugin_headers(
+  src/seg_ph_feat.h
+  )
+

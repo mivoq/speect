be1ae7a0bb00c0d85b7642ca0cce3662068ac1ea
Aby Louw <jalouw@csir.co.za>
2011-10-21 17:18:47 +0200

    Fixed archive target.

--- cmake/spctArchive.cmake
+++ cmake/spctArchive.cmake
@@ -10,7 +10,7 @@
 ##                                                                                  ##
 ######################################################################################
 
-set(SPCT_ARCHIVE_NAME ${CMAKE_PROJECT_NAME}${SPCT_VERSION})
+set(SPCT_ARCHIVE_NAME ${CMAKE_PROJECT_NAME}-${SPCT_VERSION})
 add_custom_target(dist
 		  COMMAND git archive --prefix=${SPCT_ARCHIVE_NAME}/ HEAD
 		  | gzip > ${CMAKE_BINARY_DIR}/${SPCT_ARCHIVE_NAME}.tar.gz

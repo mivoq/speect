72105dfb03b18f78854bdb232b3583b9bf41b111
Aby Louw <jalouw@csir.co.za>
2011-10-21 16:56:30 +0200

    Fixed version numbers for docs that cannot import speect.

--- docs/source/conf.c.engine.py
+++ docs/source/conf.c.engine.py
@@ -62,7 +62,7 @@ copyright = u'2011, Human Language Technologies Group, Meraka Institute, CSIR, S
 # built documents.
 #
 # The short X.Y version.
-version = "1.0"
+version = "1.0.0"
 
 # The full version, including alpha/beta/rc tags.
 release = "1.0.0"

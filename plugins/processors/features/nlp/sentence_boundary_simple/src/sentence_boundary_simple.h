84a199e60fd6b713cea32f589cd1f9dfd76232de
Aby Louw <jalouw@csir.co.za>
2012-06-28 21:47:21 +0200

    Added simple sentence boundary detection feature processor

---
+++ plugins/processors/features/nlp/sentence_boundary_simple/src/sentence_boundary_simple.h
@@ -0,0 +1,130 @@
+/************************************************************************************/
+/* Copyright (c) 2012 The Department of Arts and Culture,                           */
+/* The Government of the Republic of South Africa.                                  */
+/*                                                                                  */
+/* Contributors:  Meraka Institute, CSIR, South Africa.                             */
+/*                                                                                  */
+/* Permission is hereby granted, free of charge, to any person obtaining a copy     */
+/* of this software and associated documentation files (the "Software"), to deal    */
+/* in the Software without restriction, including without limitation the rights     */
+/* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell        */
+/* copies of the Software, and to permit persons to whom the Software is            */
+/* furnished to do so, subject to the following conditions:                         */
+/* The above copyright notice and this permission notice shall be included in       */
+/* all copies or substantial portions of the Software.                              */
+/*                                                                                  */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR       */
+/* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,         */
+/* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE      */
+/* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER           */
+/* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,    */
+/* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN        */
+/* THE SOFTWARE.                                                                    */
+/*                                                                                  */
+/************************************************************************************/
+/*                                                                                  */
+/* AUTHOR  : Aby Louw                                                               */
+/* DATE    : June 2012                                                              */
+/*                                                                                  */
+/************************************************************************************/
+/*                                                                                  */
+/* A simple sentence boundary detection feature processor.                          */
+/* Split text based on periods.                                                     */
+/*                                                                                  */
+/************************************************************************************/
+
+#ifndef _SPCT_PLUGIN_FEATPROCESSOR_SENTENCE_BOUNDARY_SIMPLE_H__
+#define _SPCT_PLUGIN_FEATPROCESSOR_SENTENCE_BOUNDARY_SIMPLE_H__
+
+
+/**
+ * @file sentence_boundary_simple.h
+ * A simple sentence boundary detection feature processor.
+ */
+
+
+/************************************************************************************/
+/*                                                                                  */
+/* Modules used                                                                     */
+/*                                                                                  */
+/************************************************************************************/
+
+#include "speect.h"
+
+
+/************************************************************************************/
+/*                                                                                  */
+/* Begin external c declaration                                                     */
+/*                                                                                  */
+/************************************************************************************/
+S_BEGIN_C_DECLS
+
+
+/************************************************************************************/
+/*                                                                                  */
+/* SSentenceBoundarySimpleFeatProc definition                                       */
+/*                                                                                  */
+/************************************************************************************/
+
+/**
+ * The simple sentence boundary detection feature processor
+ * structure. Does not add any new members, therefore exactly the same
+ * as #SFeatProcessor.
+ */
+typedef SFeatProcessor SSentenceBoundarySimpleFeatProc;
+
+
+/************************************************************************************/
+/*                                                                                  */
+/* SSentenceBoundarySimpleFeatProcClass definition                                  */
+/*                                                                                  */
+/************************************************************************************/
+
+/**
+ * Typedef of the simple sentence boundary detection feature processor
+ * class. Does not add any new methods, therefore exactly the same as
+ * #SFeatProcessorClass.
+ */
+typedef SFeatProcessorClass SSentenceBoundarySimpleFeatProcClass;
+
+
+/************************************************************************************/
+/*                                                                                  */
+/* Plug-in class registration/free                                                  */
+/*                                                                                  */
+/************************************************************************************/
+
+/**
+ * Register the #SSentenceBoundarySimpleFeatProc plug-in class with
+ * the Speect Engine object system.
+ * @private
+ *
+ * @param error Error code.
+ */
+S_LOCAL void _s_sentence_boundary_simple_class_reg(s_erc *error);
+
+
+/**
+ * Free the #SSentenceBoundarySimpleFeatProc plug-in class from the
+ * Speect Engine object system.
+ * @private
+ *
+ * @param error Error code.
+ */
+S_LOCAL void _s_sentence_boundary_simple_class_free(s_erc *error);
+
+
+/************************************************************************************/
+/*                                                                                  */
+/* End external c declaration                                                       */
+/*                                                                                  */
+/************************************************************************************/
+S_END_C_DECLS
+
+
+/**
+ * @}
+ * end documentation
+ */
+
+#endif /* _SPCT_PLUGIN_FEATPROCESSOR_SENTENCE_BOUNDARY_SIMPLE_H__ */

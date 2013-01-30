bc2f35f3be421e9e45ffbae745027c426060658a
Aby Louw <jalouw@csir.co.za>
2013-01-30 23:45:14 +0200

    Updated conf.py documentation year

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -55,7 +55,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Speect'
-copyright = u'2011, Human Language Technologies Group, Meraka Institute, CSIR, South Africa'
+copyright = u'2013, Human Language Technologies Group, Meraka Institute, CSIR, South Africa'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the

aaa1047646bac2c29eeb7b07b78fa9eefb9c27c7
Matteo Lisotto <matteo.lisotto@gmail.com>
2015-09-22 11:05:20 +0200

    Add utt_maryxml.h.

---
+++ plugins/serialization/files/utterance/maryxml/src/utt_maryxml.h
@@ -0,0 +1,122 @@
+/************************************************************************************/
+/* Copyright (c) 2015 Mivoq SRL <info@mivoq.it>                                     */
+/* Copyright (c) 2012 The Department of Arts and Culture,                           */
+/* The Government of the Republic of South Africa.                                  */
+/*                                                                                  */
+/* Contributors:  Meraka Institute, CSIR, South Africa.                             */
+/*                Matteo Lisotto                                                    */
+/*                                                                                  */
+/* Permission is hereby granted, free of charge, to any person obtaining a copy     */
+/* of this software and associated documentation files (the "Software"), to deal    */
+/* in the Software without restriction, including without limitation the rights     */
+/* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell        */
+/* copies of the Software, and to permit persons to whom the Software is            */
+/* furnished to do so, subject to the following conditions:                         */
+/* The above copyright notice and this permission notice shall be included in       */
+/* all copies or substantial portions of the Software.                              */
+/*                                                                                  */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR       */
+/* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,         */
+/* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE      */
+/* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER           */
+/* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,    */
+/* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN        */
+/* THE SOFTWARE.                                                                    */
+/*                                                                                  */
+/************************************************************************************/
+/*                                                                                  */
+/* AUTHOR  : Matteo Lisotto <matteo.lisotto@gmail.com>                              */
+/* DATE    : 21 September 2015                                                      */
+/*                                                                                  */
+/************************************************************************************/
+/*                                                                                  */
+/* SSerializedFile implementation for MaryXML format utterances.                    */
+/* Write utterances to MaryXML formatted files.                                     */
+/*                                                                                  */
+/************************************************************************************/
+
+#ifndef _SPCT_PLUGIN_SERIALIZED_MARYXML_UTTERANCES__
+#define _SPCT_PLUGIN_SERIALIZED_MARYXML_UTTERANCES__
+
+
+/**
+ * @file utt_maryxml.h
+ * SSerializedFile implementation for MaryXML format utterances.
+ */
+
+
+/**
+ * @ingroup SSerializedFile
+ * @defgroup SMaryXMLUtterance Serialized MaryXML Utterance
+ * SSerializedFile implementation for MaryXML format utterances.
+ * @{
+ */
+
+
+/************************************************************************************/
+/*                                                                                  */
+/* Modules used                                                                     */
+/*                                                                                  */
+/************************************************************************************/
+
+#include "speect.h"
+
+
+/************************************************************************************/
+/*                                                                                  */
+/* Begin external c declaration                                                     */
+/*                                                                                  */
+/************************************************************************************/
+S_BEGIN_C_DECLS
+
+
+/************************************************************************************/
+/*                                                                                  */
+/* Plug-in class registration/free                                                  */
+/*                                                                                  */
+/************************************************************************************/
+
+/**
+ * Register the plug-in class with the Speect Engine object
+ * system.
+ * @private
+ *
+ * @param error Error code.
+ */
+S_LOCAL void _s_serialized_maryxml_utt_reg(s_erc *error);
+
+
+/**
+ * Free the plug-in class from the Speect Engine object
+ * system.
+ * @private
+ *
+ * @param error Error code.
+ */
+S_LOCAL void _s_serialized_maryxml_utt_free(s_erc *error);
+
+
+/**
+ * Write an utterance in MaryXML format to the given data source.
+ *
+ * @param utt The utterance to write.
+ * @param ds The data source where to write to.
+ * @param error Error code.
+ */
+S_LOCAL void s_write_utt_maryxml(const SUtterance *utt, SDatasource *ds, s_erc *error);
+
+
+/************************************************************************************/
+/*                                                                                  */
+/* End external c declaration                                                       */
+/*                                                                                  */
+/************************************************************************************/
+S_END_C_DECLS
+
+
+/**
+ * @}
+ * end documentation
+ */
+
+#endif /* _SPCT_PLUGIN_SERIALIZED_MARYXML_UTTERANCES__ */

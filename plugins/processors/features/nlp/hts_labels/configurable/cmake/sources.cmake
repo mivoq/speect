c86cae43325b5857584d14e0225a5e3f31ce664d
Simone Daminato <daminato.simone@gmail.com>
2016-01-15 19:32:40 +0100

    Created sources.cmake file for configurable hts labeler

---
+++ plugins/processors/features/nlp/hts_labels/configurable/cmake/sources.cmake
@@ -0,0 +1,27 @@
+######################################################################################
+##                                                                                  ##
+## AUTHOR  : Simone Daminato                                                        ##
+## DATE    : 22 December 2015                                                       ##
+##                                                                                  ##
+######################################################################################
+##                                                                                  ##
+## Source files for HTS Labels "configurable" feature processor plug-in             ##
+##                                                                                  ##
+##                                                                                  ##
+######################################################################################
+
+
+######## source files ##################
+
+speect_plugin_sources(
+  src/plugin.c
+  src/hts_labels.c
+  )
+ 
+
+######## header files ##################
+
+speect_plugin_headers(
+  src/hts_labels.h
+  )
+

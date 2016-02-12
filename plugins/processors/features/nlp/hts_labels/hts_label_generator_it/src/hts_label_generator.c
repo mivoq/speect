decd63712629fc531e01f79096f3987bb31d253f
Simone Daminato <daminato.simone@gmail.com>
2016-02-12 21:00:38 +0100

    Created macros for features extrapolation in hts_label_generator.c

--- plugins/processors/features/nlp/hts_labels/hts_label_generator_it/src/hts_label_generator.c
+++ plugins/processors/features/nlp/hts_labels/hts_label_generator_it/src/hts_label_generator.c
@@ -52,6 +52,130 @@
 
 #define MAX_PHONE_NAME_LENGTH 8
 
+/**
+ * @hideinitializer
+ * @def EXTRACTFEATURE
+ * Exctract from @a data the feature with key @a featureName.
+ * The feature then is unboxed using the function @a unboxingFunction,
+ * a string is generated as defined by @a outputString, and appended to
+ * @a appendTo.
+ * If the feature @a featureName is not present, @a defaultOutput is appended
+ * to @a appendTo instead.
+ *
+ * @param data Data container (type #SHTSLabelsDataCollectorFeatProc*).
+ * @param featureName Key of the feature (type #char*).
+ * @param unboxingFunction Function used to get data from the resulting SObject (type #void*).
+ * @param outputString String to format the feature (type #char*).
+ * @param defaultOutput Default string output, used if there isn't the given feature (type #char*).
+ * @param appendTo String on which we append the output (type #char*).
+ * @param ERROR Pointer to error code variable to set (type #s_erc*).
+ */
+#define EXTRACTFEATURE(data, featureName, unboxingFunction, outputString, defaultOutput, appendTo, error)	\
+do {														\
+	char *tmp = NULL;											\
+	do {													\
+		const SObject *feature;										\
+		s_bool isPresent;											\
+		S_CLR_ERR(error);										\
+														\
+		isPresent= S_HTSLABELSDATACOLLECTOR_CALL(data, has_feature)(data, featureName, error);		\
+		if (S_CHK_ERR(error, S_CONTERR,									\
+					"EXTRACTFEATURE",							\
+					"Call to \"SHTSLabelDataCollectorFeatureIsPresent\" failed"))		\
+			break;											\
+		if (isPresent)											\
+		{												\
+			feature = S_HTSLABELSDATACOLLECTOR_CALL(data, get_feature)(data, featureName, error);	\
+			if (S_CHK_ERR(error, S_CONTERR,								\
+					"EXTRACTFEATURE",								\
+					"Call to \"SHTSLabelDataCollectorGetFeature\" failed"))			\
+				break;										\
+			s_asprintf(&tmp, error, outputString, unboxingFunction(feature, error));		\
+		}												\
+		else												\
+		{												\
+			s_asprintf(&tmp, error, defaultOutput);							\
+			if (S_CHK_ERR(error, S_CONTERR,								\
+					"EXTRACTFEATURE",							\
+					"Call to \"s_asprintf\" failed"))					\
+			break;											\
+		}												\
+		s_sappend(&appendTo, tmp, error);								\
+		if (S_CHK_ERR(error, S_CONTERR,									\
+					"EXTRACTFEATURE",							\
+					"Call to \"s_sappend\" failed"))					\
+			break;											\
+	} while(0);												\
+	if (tmp != NULL) S_FREE(tmp);										\
+} while(0);
+
+
+/**
+ * @hideinitializer
+ * @def EXTRACTPHONEME
+ * Exctract from @a data the phoneme with key @a featureName.
+ * The feature then is unboxed, fixed using @c CheckPhoneme, and a string is generated
+ * as defined by @a outputString, and appended to @a appendTo.
+ * If the feature @a featureName is not present,  @a nullPhoneme (took from @c self)
+ * is used instead.
+ *
+ * @param self self pointer (type #SHTSLabelsGeneratorItFeatProc*)
+ * @param data Data container (type #SHTSLabelsDataCollectorFeatProc*).
+ * @param featureName Key of the feature (type #char*).
+ * @param outputString String to format the feature (type #char*).
+ * @param appendTo String on which we append the output (type #char*).
+ * @param ERROR Pointer to error code variable to set (type #s_erc*).
+ */
+#define EXTRACTPHONEME(self, data, featureName, outputString, appendTo, error)					\
+do {														\
+	char *tmp = NULL;											\
+	do {													\
+		const SObject *feature;										\
+		char *phone;											\
+		s_bool isPresent;										\
+		S_CLR_ERR(error);										\
+														\
+		isPresent= S_HTSLABELSDATACOLLECTOR_CALL(data, has_feature)(data, featureName, error);		\
+		if (S_CHK_ERR(error, S_CONTERR,									\
+					"EXTRACTFEATURE",							\
+					"Call to \"SHTSLabelDataCollectorFeatureIsPresent\" failed"))		\
+			break;											\
+		if (isPresent)											\
+		{												\
+			feature = S_HTSLABELSDATACOLLECTOR_CALL(data, get_feature)(data, featureName, error);	\
+			if (S_CHK_ERR(error, S_CONTERR,								\
+					"EXTRACTFEATURE",							\
+					"Call to \"SHTSLabelDataCollectorGetFeature\" failed"))			\
+				break;										\
+			phone = (char*) SObjectGetString(feature, error);					\
+			if (S_CHK_ERR(error, S_CONTERR,								\
+					"EXTRACTFEATURE",							\
+					"Call to \"SObjectGetString\" failed"))					\
+				break;										\
+			CheckPhoneme(self, &phone, error);							\
+			if (S_CHK_ERR(error, S_CONTERR,								\
+					"EXTRACTFEATURE",							\
+					"Call to \"CheckPhoneme\" failed"))					\
+				break;										\
+			s_asprintf(&tmp, error, outputString, phone);						\
+		}												\
+		else												\
+		{												\
+			s_asprintf(&tmp, error, outputString, self->nullPhoneme);				\
+			if (S_CHK_ERR(error, S_CONTERR,								\
+					"EXTRACTFEATURE",							\
+					"Call to \"s_asprintf\" failed"))					\
+			break;											\
+		}												\
+		s_sappend(&appendTo, tmp, error);								\
+		if (S_CHK_ERR(error, S_CONTERR,									\
+					"EXTRACTFEATURE",							\
+					"Call to \"s_sappend\" failed"))					\
+			break;											\
+	} while(0);												\
+	if (tmp != NULL) S_FREE(tmp);										\
+} while(0);
+
 
 /************************************************************************************/
 /*                                                                                  */
@@ -137,7 +261,7 @@ static void LoadConfiguration(SHTSLabelsGeneratorItFeatProc *self, const SItem *
 				  "Item voice is NULL, voice is required to get phoneset");
 		return;
 	}
-	
+
 	/* get the hunpos data map */
 	labelsGenerator_data = S_MAP(SVoiceGetFeature(voice, "labels generator data", error));
 	if (S_CHK_ERR(error, S_CONTERR,
@@ -152,7 +276,7 @@ static void LoadConfiguration(SHTSLabelsGeneratorItFeatProc *self, const SItem *
 				  "Failed to get \"labels generator data\" map from voice features");
 		return;
 	}
-	
+
 	self->specialPhones = S_MAP(SMapGetObject(labelsGenerator_data, "special phones it", error));
 	if (S_CHK_ERR(error, S_CONTERR,
 				  "LoadConfiguration",
@@ -165,7 +289,7 @@ static void LoadConfiguration(SHTSLabelsGeneratorItFeatProc *self, const SItem *
 				  "Failed to get \"special phones it\" map from voice features");
 		return;
 	}
-	
+
 	self->nullPhoneme = SMapGetString(labelsGenerator_data, "null phoneme it", error);
 	if (S_CHK_ERR(error, S_CONTERR,
 				  "LoadConfiguration",
@@ -178,20 +302,20 @@ static void LoadConfiguration(SHTSLabelsGeneratorItFeatProc *self, const SItem *
 				  "Failed to get \"null phoneme it\" string from voice features");
 		return;
 	}
-	
+
 }
 
 static void CheckPhoneme(SHTSLabelsGeneratorItFeatProc *self, char ** phone, s_erc *error)
 {
 	S_CLR_ERR(error);
-	
+
 	*phone = (char*) SMapGetStringDef(self->specialPhones, *phone, *phone, error);
 }
 
 static void Init(void *obj, s_erc *error)
 {
 	S_CLR_ERR(error);
-	
+
 	SHTSLabelsGeneratorItFeatProc* self = (SHTSLabelsGeneratorItFeatProc*) obj;
 	self->specialPhones = NULL;
 	self->nullPhoneme = NULL;
@@ -200,14 +324,14 @@ static void Init(void *obj, s_erc *error)
 static void Dispose(void *obj, s_erc *error)
 {
 	S_CLR_ERR(error);
-	
+
 	SObjectDecRef(obj);
 }
 
 static void Destroy(void *obj, s_erc *error)
 {
 	S_CLR_ERR(error);
-	
+
 	SHTSLabelsGeneratorItFeatProc* self = (SHTSLabelsGeneratorItFeatProc*) obj;
 	if (self->specialPhones != NULL)
 	{
@@ -227,9 +351,9 @@ static SObject *Run(const SFeatProcessor *self, const SItem *item,
 	char *tmp = NULL;
 	char *phone = NULL;
 	s_bool isPresent;
-	
+
 	S_CLR_ERR(error);
-	
+
 	/* load configuration if it's the first run */
 	if (labelGenerator->specialPhones == NULL || labelGenerator->nullPhoneme == NULL)
 	{
@@ -249,9 +373,9 @@ static SObject *Run(const SFeatProcessor *self, const SItem *item,
 			goto quit_error;
 		}
 	}
-	
+
 	SItem* segItem = SItemAs(item, "Segment", error);
-	
+
 	data = (SHTSLabelsDataCollectorFeatProc *) SItemPathToFeatProc(segItem, "hts_label_data_collector", error);
 	if (S_CHK_ERR(error, S_CONTERR,
 				  "Run",
@@ -273,7 +397,7 @@ static SObject *Run(const SFeatProcessor *self, const SItem *item,
 				  "labelsGenerator's has_feature method is NULL.");
 		goto quit_error;
 	}
-	
+
 	if (! S_HTSLABELSDATACOLLECTOR_METH_VALID(data, get_feature))
 	{
 		S_CTX_ERR(error, S_FAILURE,
@@ -281,317 +405,83 @@ static SObject *Run(const SFeatProcessor *self, const SItem *item,
 				  "labelsGenerator's get_feature method is NULL.");
 		goto quit_error;
 	}
-	
+
 	/**** phone context ****/
 	/* p1 */
-	isPresent = S_HTSLABELSDATACOLLECTOR_CALL(data, has_feature)(data, "p.p.phone", error);
+	EXTRACTPHONEME(labelGenerator, data, "p.p.phone", "%s", resultString, error);
 	if (S_CHK_ERR(error, S_CONTERR,
 				  "Run",
-				  "Call to \"SHTSLabelDataCollectorFeatureIsPresent\" failed"))
+				  "Execution of \"EXTRACTPHONEME\" failed"))
 		goto quit_error;
-	if (isPresent)
-	{
-		feature = S_HTSLABELSDATACOLLECTOR_CALL(data, get_feature)(data, "p.p.phone", error);
-		if (S_CHK_ERR(error, S_CONTERR,
-				  "Run",
-				  "Call to \"SHTSLabelDataCollectorGetFeature\" failed"))
-			goto quit_error;
-		phone = (char*) SObjectGetString(feature, error);
-		if (S_CHK_ERR(error, S_CONTERR,
-				  "Run",
-				  "Call to \"SObjectGetString\" failed"))
-			goto quit_error;
-		CheckPhoneme(labelGenerator, &phone, error);
-		if (S_CHK_ERR(error, S_CONTERR,
-				  "Run",
-				  "Call to \"CheckPhoneme\" failed"))
-			goto quit_error;
-		
-		s_asprintf(&tmp, error, "%s", phone);
-	}
-	else
-	{
-		s_asprintf(&tmp, error, "%s", labelGenerator->nullPhoneme);
-		if (S_CHK_ERR(error, S_CONTERR,
-				"Run",
-				"Call to \"s_asprintf\" failed"))
-		goto quit_error;
-	}
-	s_sappend(&resultString, tmp, error);
-	if (S_CHK_ERR(error, S_CONTERR,
-				  "Run",
-				  "Call to \"s_sappend\" failed"))
-		goto quit_error;
-	S_FREE(tmp);
-	
+
 	/* p2 */
-	isPresent = S_HTSLABELSDATACOLLECTOR_CALL(data, has_feature)(data, "p.phone", error);
+	EXTRACTPHONEME(labelGenerator, data, "p.phone", "^%s", resultString, error);
 	if (S_CHK_ERR(error, S_CONTERR,
 				  "Run",
-				  "Call to \"SHTSLabelDataCollectorFeatureIsPresent\" failed"))
+				  "Execution of \"EXTRACTPHONEME\" failed"))
 		goto quit_error;
-	if (isPresent)
-	{
-		feature = S_HTSLABELSDATACOLLECTOR_CALL(data, get_feature)(data, "p.phone", error);
-		if (S_CHK_ERR(error, S_CONTERR,
-				  "Run",
-				  "Call to \"SHTSLabelDataCollectorGetFeature\" failed"))
-			goto quit_error;
-		phone = (char*) SObjectGetString(feature, error);
-		if (S_CHK_ERR(error, S_CONTERR,
-				  "Run",
-				  "Call to \"SObjectGetString\" failed"))
-			goto quit_error;
-		CheckPhoneme(labelGenerator, &phone, error);
-		if (S_CHK_ERR(error, S_CONTERR,
-				  "Run",
-				  "Call to \"CheckPhoneme\" failed"))
-			goto quit_error;
-		
-		s_asprintf(&tmp, error, "^%s", phone);
-	}
-	else
-	{
-		s_asprintf(&tmp, error, "^%s", labelGenerator->nullPhoneme);
-		if (S_CHK_ERR(error, S_CONTERR,
-				"Run",
-				"Call to \"s_asprintf\" failed"))
-		goto quit_error;
-	}
-	s_sappend(&resultString, tmp, error);
-	if (S_CHK_ERR(error, S_CONTERR,
-				  "Run",
-				  "Call to \"s_sappend\" failed"))
-		goto quit_error;
-	S_FREE(tmp);
-	
+
 	/* p3 */
-	isPresent = S_HTSLABELSDATACOLLECTOR_CALL(data, has_feature)(data, "phone", error);
+	EXTRACTPHONEME(labelGenerator, data, "phone", "-%s", resultString, error);
 	if (S_CHK_ERR(error, S_CONTERR,
 				  "Run",
-				  "Call to \"SHTSLabelDataCollectorFeatureIsPresent\" failed"))
+				  "Execution of \"EXTRACTPHONEME\" failed"))
 		goto quit_error;
-	if (isPresent)
-	{
-		feature = S_HTSLABELSDATACOLLECTOR_CALL(data, get_feature)(data, "phone", error);
-		if (S_CHK_ERR(error, S_CONTERR,
-				  "Run",
-				  "Call to \"SHTSLabelDataCollectorGetFeature\" failed"))
-			goto quit_error;
-		phone = (char*) SObjectGetString(feature, error);
-		if (S_CHK_ERR(error, S_CONTERR,
-				  "Run",
-				  "Call to \"SObjectGetString\" failed"))
-			goto quit_error;
-		CheckPhoneme(labelGenerator, &phone, error);
-		if (S_CHK_ERR(error, S_CONTERR,
-				  "Run",
-				  "Call to \"CheckPhoneme\" failed"))
-			goto quit_error;
-		
-		s_asprintf(&tmp, error, "-%s", phone);
-	}
-	else
-	{
-		s_asprintf(&tmp, error, "-%s", labelGenerator->nullPhoneme);
-		if (S_CHK_ERR(error, S_CONTERR,
-				"Run",
-				"Call to \"s_asprintf\" failed"))
-		goto quit_error;
-	}
-	s_sappend(&resultString, tmp, error);
-	if (S_CHK_ERR(error, S_CONTERR,
-				  "Run",
-				  "Call to \"s_sappend\" failed"))
-		goto quit_error;
-	S_FREE(tmp);
-	
+
 	/* p4 */
-	isPresent = S_HTSLABELSDATACOLLECTOR_CALL(data, has_feature)(data, "n.phone", error);
+	EXTRACTPHONEME(labelGenerator, data, "n.phone", "+%s", resultString, error);
 	if (S_CHK_ERR(error, S_CONTERR,
 				  "Run",
-				  "Call to \"SHTSLabelDataCollectorFeatureIsPresent\" failed"))
+				  "Execution of \"EXTRACTPHONEME\" failed"))
 		goto quit_error;
-	if (isPresent)
-	{
-		feature = S_HTSLABELSDATACOLLECTOR_CALL(data, get_feature)(data, "n.phone", error);
-		if (S_CHK_ERR(error, S_CONTERR,
-				  "Run",
-				  "Call to \"SHTSLabelDataCollectorGetFeature\" failed"))
-			goto quit_error;
-		phone = (char*) SObjectGetString(feature, error);
-		if (S_CHK_ERR(error, S_CONTERR,
-				  "Run",
-				  "Call to \"SObjectGetString\" failed"))
-			goto quit_error;
-		CheckPhoneme(labelGenerator, &phone, error);
-		if (S_CHK_ERR(error, S_CONTERR,
-				  "Run",
-				  "Call to \"CheckPhoneme\" failed"))
-			goto quit_error;
-		
-		s_asprintf(&tmp, error, "+%s", phone);
-	}
-	else
-	{
-		s_asprintf(&tmp, error, "+%s", labelGenerator->nullPhoneme);
-		if (S_CHK_ERR(error, S_CONTERR,
-				"Run",
-				"Call to \"s_asprintf\" failed"))
-		goto quit_error;
-	}
-	s_sappend(&resultString, tmp, error);
-	if (S_CHK_ERR(error, S_CONTERR,
-				  "Run",
-				  "Call to \"s_sappend\" failed"))
-		goto quit_error;
-	S_FREE(tmp);
-	
+
 	/* p5 */
-	isPresent = S_HTSLABELSDATACOLLECTOR_CALL(data, has_feature)(data, "n.n.phone", error);
+	EXTRACTPHONEME(labelGenerator, data, "n.n.phone", "=%s||", resultString, error);
 	if (S_CHK_ERR(error, S_CONTERR,
 				  "Run",
-				  "Call to \"SHTSLabelDataCollectorFeatureIsPresent\" failed"))
+				  "Execution of \"EXTRACTPHONEME\" failed"))
 		goto quit_error;
-	if (isPresent)
-	{
-		feature = S_HTSLABELSDATACOLLECTOR_CALL(data, get_feature)(data, "n.n.phone", error);
-		if (S_CHK_ERR(error, S_CONTERR,
-				  "Run",
-				  "Call to \"SHTSLabelDataCollectorGetFeature\" failed"))
-			goto quit_error;
-		phone = (char*) SObjectGetString(feature, error);
-		if (S_CHK_ERR(error, S_CONTERR,
-				  "Run",
-				  "Call to \"SObjectGetString\" failed"))
-			goto quit_error;
-		CheckPhoneme(labelGenerator, &phone, error);
-		if (S_CHK_ERR(error, S_CONTERR,
-				  "Run",
-				  "Call to \"CheckPhoneme\" failed"))
-			goto quit_error;
-		
-		s_asprintf(&tmp, error, "=%s||", phone);
-	}
-	else
-	{
-		s_asprintf(&tmp, error, "=%s||", labelGenerator->nullPhoneme);
-		if (S_CHK_ERR(error, S_CONTERR,
-				"Run",
-				"Call to \"s_asprintf\" failed"))
-		goto quit_error;
-	}
-	s_sappend(&resultString, tmp, error);
-	if (S_CHK_ERR(error, S_CONTERR,
-				  "Run",
-				  "Call to \"s_sappend\" failed"))
-		goto quit_error;
-	S_FREE(tmp);
-	
-	
-	
+
+
+
 	/* f48: syls_to_next_stressed */
-	isPresent = S_HTSLABELSDATACOLLECTOR_CALL(data, has_feature)(data, "syls.to.next.stress", error);
+	EXTRACTFEATURE(data, "syls.to.next.stress", SObjectGetInt, "f48=%d|", "f48=0|", resultString, error);
 	if (S_CHK_ERR(error, S_CONTERR,
 				  "Run",
-				  "Call to \"SHTSLabelDataCollectorFeatureIsPresent\" failed"))
+				  "Execution of \"EXTRACTFEATURE\" failed"))
 		goto quit_error;
-	if (isPresent)
-	{
-		feature = S_HTSLABELSDATACOLLECTOR_CALL(data, get_feature)(data, "syls.to.next.stress", error);
-		if (S_CHK_ERR(error, S_CONTERR,
-				  "Run",
-				  "Call to \"SHTSLabelDataCollectorGetFeature\" failed"))
-			goto quit_error;
-		
-		s_asprintf(&tmp, error, "f48=%d|", SObjectGetInt(feature, error));
-	}
-	else
-	{
-		s_asprintf(&tmp, error, "f48=0|");
-		if (S_CHK_ERR(error, S_CONTERR,
-				"Run",
-				"Call to \"s_asprintf\" failed"))
-		goto quit_error;
-	}
-	s_sappend(&resultString, tmp, error);
-	if (S_CHK_ERR(error, S_CONTERR,
-				  "Run",
-				  "Call to \"s_sappend\" failed"))
-		goto quit_error;
-	S_FREE(tmp);
-	
+
 	/* f6: next_accent */
-	isPresent = S_HTSLABELSDATACOLLECTOR_CALL(data, has_feature)(data, "n.accent", error);
+	EXTRACTFEATURE(data, "n.accent", SObjectGetInt, "f6=%d|", "f6=0|", resultString, error);
 	if (S_CHK_ERR(error, S_CONTERR,
 				  "Run",
-				  "Call to \"SHTSLabelDataCollectorFeatureIsPresent\" failed"))
+				  "Execution of \"EXTRACTFEATURE\" failed"))
 		goto quit_error;
-	if (isPresent)
-	{
-		feature = S_HTSLABELSDATACOLLECTOR_CALL(data, get_feature)(data, "n.accent", error);
-		if (S_CHK_ERR(error, S_CONTERR,
-				  "Run",
-				  "Call to \"SHTSLabelDataCollectorGetFeature\" failed"))
-			goto quit_error;
-		
-		s_asprintf(&tmp, error, "f6=%d|", SObjectGetInt(feature, error));
-	}
-	else
-	{
-		s_asprintf(&tmp, error, "f6=0|");
-		if (S_CHK_ERR(error, S_CONTERR,
-				"Run",
-				"Call to \"s_asprintf\" failed"))
-		goto quit_error;
-	}
-	s_sappend(&resultString, tmp, error);
+
+	/* f22: pos in syl */
+	EXTRACTFEATURE(data, "phones.from.syl.start", SObjectGetInt, "f22=%d|", "f22=0|", resultString, error);
 	if (S_CHK_ERR(error, S_CONTERR,
 				  "Run",
-				  "Call to \"s_sappend\" failed"))
+				  "Execution of \"EXTRACTFEATURE\" failed"))
 		goto quit_error;
-	S_FREE(tmp);
-	
-	/* f22: next_accent */
-	isPresent = S_HTSLABELSDATACOLLECTOR_CALL(data, has_feature)(data, "n.accent", error);
+
+	/* f22: pos in syl */
+	EXTRACTFEATURE(data, "phones.from.syl.start", SObjectGetInt, "f22=%d|", "f22=0|", resultString, error);
 	if (S_CHK_ERR(error, S_CONTERR,
 				  "Run",
-				  "Call to \"SHTSLabelDataCollectorFeatureIsPresent\" failed"))
+				  "Execution of \"EXTRACTFEATURE\" failed"))
 		goto quit_error;
-	if (isPresent)
-	{
-		feature = S_HTSLABELSDATACOLLECTOR_CALL(data, get_feature)(data, "n.accent", error);
-		if (S_CHK_ERR(error, S_CONTERR,
-				  "Run",
-				  "Call to \"SHTSLabelDataCollectorGetFeature\" failed"))
-			goto quit_error;
-		
-		s_asprintf(&tmp, error, "f6=%d|", SObjectGetInt(feature, error));
-	}
-	else
-	{
-		s_asprintf(&tmp, error, "f6=0|");
-		if (S_CHK_ERR(error, S_CONTERR,
-				"Run",
-				"Call to \"s_asprintf\" failed"))
-		goto quit_error;
-	}
-	s_sappend(&resultString, tmp, error);
-	if (S_CHK_ERR(error, S_CONTERR,
-				  "Run",
-				  "Call to \"s_sappend\" failed"))
-		goto quit_error;
-	S_FREE(tmp);
-	
+
+
 	return SObjectSetString(resultString, error);
-	
+
 	/* error cleanup */
 quit_error:
 	S_DELETE(data, "Run", error);
 	S_FREE(tmp);
 	S_FREE(resultString);
 	return NULL;
-	
+
 	S_UNUSED(self);
 }
 

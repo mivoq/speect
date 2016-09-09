8aa7f7fd72adf383010537658b3f7f8e93b74189
fedingo <fedingo@gmail.com>
2016-09-09 09:43:44 +0200

    "Updated syl_accent_out.c after the introduction of init method to SFeatProcessorClass."

--- plugins/processors/features/nlp/syllable_accent_out/src/syl_accent_out.c
+++ plugins/processors/features/nlp/syllable_accent_out/src/syl_accent_out.c
@@ -284,5 +284,6 @@ static SSylAccentOutFeatProcClass SylAccentOutFeatProcClass =
 		NULL,            /* copy    */
 	},
 	/* SFeatProcessorClass */
+	NULL,               /* initialize */
 	Run                  /* run     */
 };

5126386d4e40261b720e003588fa7f2fdc9200e7
fedingo <fedingo@gmail.com>
2016-09-14 14:44:55 +0200

    Added words_from_phrase_end plugin.

---
+++ plugins/processors/features/nlp/words_from_phrase_end/src/words_from_phrase_end.h
@@ -0,0 +1,136 @@
+/************************************************************************************/
+/* Copyright (c) Mivoq SRL                                                          */
+/*                                                                                  */
+/* Contributors:  Federico Rossetto, Giulio Paci                                    */
+/*                                                                                  *//*                                                                                  */
+/* Permission is hereby granted, free of charge, to any person obtaining a copy     */
+/* of this software and associated documentation files (the "Software"), to deal    */
+/* in the Software without restriction, including without limitation the rights     */
+/* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell        */
+/* copies of the Software, and to permit persons to whom the Software is            */
+/* furnished to do so, subject to the following conditions:                         */
+/* The above copyright notice and this permission notice shall be included in       */
+/* all copies or substantial portions of the Software.                              */
+/*                                                                                  */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR       */
+/* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,         */
+/* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE      */
+/* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER           */
+/* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,    */
+/* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN        */
+/* THE SOFTWARE.                                                                    */
+/*                                                                                  */
+/************************************************************************************/
+/*                                                                                  */
+/* AUTHOR  : Federico Rossetto                                                      */
+/* DATE    : September 2016                                                         */
+/*                                                                                  */
+/************************************************************************************/
+/*                                                                                  */
+/* Phrase number of words feature processor.                                        */
+/*                                                                                  */
+/*                                                                                  */
+/************************************************************************************/
+
+#ifndef _SPCT_PLUGIN_FEATPROCESSOR_WORDS_FROM_PHRASE_END_H__
+#define _SPCT_PLUGIN_FEATPROCESSOR_WORDS_FROM_PHRASE_END_H__
+
+
+/**
+ * @file words_from_phrase_end.h
+ * Phrase number of words feature processor.
+ */
+
+
+/**
+ * @ingroup SFeatureProc
+ * @defgroup SWordsFromPhraseEndFeatProc Words from phrase start
+ * @{
+ */
+
+
+/************************************************************************************/
+/*                                                                                  */
+/* Modules used                                                                     */
+/*                                                                                  */
+/************************************************************************************/
+
+#include "speect.h"
+
+
+/************************************************************************************/
+/*                                                                                  */
+/* Begin external c declaration                                                     */
+/*                                                                                  */
+/************************************************************************************/
+S_BEGIN_C_DECLS
+
+
+/************************************************************************************/
+/*                                                                                  */
+/* SPhraseNumWordsFeatProc definition                                               */
+/*                                                                                  */
+/************************************************************************************/
+
+/**
+ * The number of words in phrase feature processor structure. Does
+ * not add any new members, therefore exactly the same as
+ * #SFeatProcessor.
+ */
+typedef SFeatProcessor SWordsFromPhraseEndFeatProc;
+
+
+/************************************************************************************/
+/*                                                                                  */
+/* SPhraseNumWordsFeatProcClass definition                                          */
+/*                                                                                  */
+/************************************************************************************/
+
+/**
+ * Typedef of the number of words in phrase feature processor
+ * class. Does not add any new methods, therefore exactly the same as
+ * #SFeatProcessorClass.
+ */
+typedef SFeatProcessorClass SWordsFromPhraseEndFeatProcClass;
+
+
+/************************************************************************************/
+/*                                                                                  */
+/* Plug-in class registration/free                                                  */
+/*                                                                                  */
+/************************************************************************************/
+
+/**
+ * Register the #SPhraseNumWordsFeatProc plug-in class with the Speect Engine
+ * object system.
+ * @private
+ *
+ * @param error Error code.
+ */
+S_LOCAL void _s_words_from_phrase_end_class_reg(s_erc *error);
+
+
+/**
+ * Free the #SPhraseNumWordsFeatProc plug-in class from the Speect Engine
+ * object system.
+ * @private
+ *
+ * @param error Error code.
+ */
+S_LOCAL void _s_words_from_phrase_end_class_free(s_erc *error);
+
+
+/************************************************************************************/
+/*                                                                                  */
+/* End external c declaration                                                       */
+/*                                                                                  */
+/************************************************************************************/
+S_END_C_DECLS
+
+
+/**
+ * @}
+ * end documentation
+ */
+
+#endif /* _SPCT_PLUGIN_FEATPROCESSOR_WORDS_FROM_PHRASE_END_H__ */

31bbdab6d457e28cf9add266287678e48a40228d
fedingo <fedingo@gmail.com>
2016-09-27 15:38:26 +0200

    Added syls_acc_from_start plugin.

---
+++ plugins/processors/features/nlp/syl_acc_in_phrase/syls_acc_from_start/cmake/sources.cmake
@@ -0,0 +1,27 @@
+######################################################################################
+##                                                                                  ##
+## AUTHOR  : Federico Rossetto                                                      ##
+## DATE    : 8 September 2016                                                       ##
+##                                                                                  ##
+######################################################################################
+##                                                                                  ##
+## Source files for syllable accented from phrase start feature processor plug-in   ##
+##                                                                                  ##
+##                                                                                  ##
+######################################################################################
+
+
+######## source files ##################
+
+speect_plugin_sources(
+  src/plugin.c
+  src/syls_acc_from_start.c
+  )
+
+
+######## header files ##################
+
+speect_plugin_headers(
+  src/syls_acc_from_start.h
+  )
+
---
+++ plugins/processors/features/nlp/syl_acc_in_phrase/syls_acc_to_end/cmake/sources.cmake
@@ -0,0 +1,27 @@
+######################################################################################
+##                                                                                  ##
+## AUTHOR  : Federico Rossetto                                                      ##
+## DATE    : 8 September 2016                                                       ##
+##                                                                                  ##
+######################################################################################
+##                                                                                  ##
+## Source files for syllable accented to phrase end feature processor plug-in       ##
+##                                                                                  ##
+##                                                                                  ##
+######################################################################################
+
+
+######## source files ##################
+
+speect_plugin_sources(
+  src/plugin.c
+  src/syls_acc_to_end.c
+  )
+
+
+######## header files ##################
+
+speect_plugin_headers(
+  src/syls_acc_to_end.h
+  )
+

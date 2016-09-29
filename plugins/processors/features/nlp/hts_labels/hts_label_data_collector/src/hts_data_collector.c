70f6e1841c244a8cbc0ddf2addd11cea86b00d20
fedingo <fedingo@gmail.com>
2016-09-29 17:17:19 +0200

    Enabled the next.punc feature in hts_data_collector.c.

--- plugins/processors/features/nlp/hts_labels/hts_label_data_collector/src/hts_data_collector.c
+++ plugins/processors/features/nlp/hts_labels/hts_label_data_collector/src/hts_data_collector.c
@@ -1515,6 +1515,23 @@ static void create_word_context(SELFPARAMETERTYPE *self, const SItem *item, s_er
 
 	}
 
+	/* the next punctuation symbol */
+	dFeat = SItemPathToFeatProc(item, "R:SylStructure.parent.parent.R:Word.next_punc", error);
+	if (S_CHK_ERR(error, S_CONTERR,
+		      "create_word_context",
+		      "Call to \"SItemPathToFeatProc\" failed"))
+		goto word_context_cleanup;
+
+	if (dFeat != NULL)
+	{
+		SHTSLabelDataCollectorSetFeature(self, "next.punc", dFeat, error);
+		if (S_CHK_ERR(error, S_CONTERR,
+					  "create_word_context",
+					  "Call to \"SHTSLabelDataCollectorSetFeature\" failed"))
+			goto word_context_cleanup;
+
+	}
+
 	return;
 
 word_context_cleanup:
@@ -1631,6 +1648,22 @@ static void create_word_context_pause(SELFPARAMETERTYPE *self, const SItem *item
 
 	}
 
+	/* the next punctuation symbol */
+	dFeat = SItemPathToFeatProc(item, "R:SylStructure.parent.parent.R:Word.next_punc", error);
+	if (S_CHK_ERR(error, S_CONTERR,
+		      "create_word_context_pause",
+		      "Call to \"SItemPathToFeatProc\" failed"))
+		goto word_context_pause_cleanup;
+
+	if (dFeat != NULL)
+	{
+		SHTSLabelDataCollectorSetFeature(self, "next.punc", dFeat, error);
+		if (S_CHK_ERR(error, S_CONTERR,
+					  "create_word_context_pause",
+					  "Call to \"SHTSLabelDataCollectorSetFeature\" failed"))
+			goto word_context_pause_cleanup;
+
+	}
 
 word_context_pause_cleanup:
 	if (dFeat != NULL)

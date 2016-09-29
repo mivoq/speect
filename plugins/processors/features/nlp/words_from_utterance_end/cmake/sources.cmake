6ec1c9fe4c2d8c67d5b554ded67138767cfdc754
fedingo <fedingo@gmail.com>
2016-09-29 17:05:55 +0200

    Added the words_from_utterance_end plugin.

---
+++ plugins/processors/features/nlp/words_from_utterance_end/cmake/sources.cmake
@@ -0,0 +1,27 @@
+######################################################################################
+##                                                                                  ##
+## AUTHOR  : Federico Rossetto                                                      ##
+## DATE    : 8 September 2016                                                       ##
+##                                                                                  ##
+######################################################################################
+##                                                                                  ##
+## Source files for Phrase num words feature processor plug-in                      ##
+##                                                                                  ##
+##                                                                                  ##
+######################################################################################
+
+
+######## source files ##################
+
+speect_plugin_sources(
+  src/plugin.c
+  src/words_from_utterance_end.c
+  )
+
+
+######## header files ##################
+
+speect_plugin_headers(
+  src/words_from_utterance_end.h
+  )
+

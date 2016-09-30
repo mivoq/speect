9b86081262fcbec2f44d646edba8a1341397a9a6
fedingo <fedingo@gmail.com>
2016-09-30 13:56:45 +0200

    Added the next_accent plugin.

---
+++ plugins/processors/features/nlp/accent/next_accent/cmake/sources.cmake
@@ -0,0 +1,27 @@
+######################################################################################
+##                                                                                  ##
+## AUTHOR  : Federico Rossetto                                                      ##
+## DATE    : 8 September 2016                                                      ##
+##                                                                                  ##
+######################################################################################
+##                                                                                  ##
+## Source files for Phrase type feature processor plug-in                           ##
+##                                                                                  ##
+##                                                                                  ##
+######################################################################################
+
+
+######## source files ##################
+
+speect_plugin_sources(
+  src/plugin.c
+  src/next_accent.c
+  )
+
+
+######## header files ##################
+
+speect_plugin_headers(
+  src/next_accent.h
+  )
+

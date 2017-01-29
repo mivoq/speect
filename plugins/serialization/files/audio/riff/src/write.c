5957e5515ef72af2c46a5ce92d9997df9eecebe2
Cristian Andrighetto <cristian_andrighetto94@virgilio.it>
2017-01-29 22:03:30 +0100

    Added s_write_audio_riff_16_to_datasource function implementation in write.c

--- plugins/serialization/files/audio/riff/src/write.c
+++ plugins/serialization/files/audio/riff/src/write.c
@@ -222,5 +222,159 @@ quit:
 		S_FREE(samples);
 
 	if (ds != NULL)
-		S_DELETE(ds, "s_read_lexicon_json", error);
+		S_DELETE(ds, "s_write_audio_riff_16", error);
+}
+
+S_LOCAL void s_write_audio_riff_16_to_datasource(const SAudio *audio, SDatasource *ds, s_erc *error)
+{
+	const char *info;
+	uint32 dword;
+	uint16 word;
+	sint16 *samples = NULL;
+	uint32 counter;
+
+
+	S_CLR_ERR(error);
+
+
+	/* riff is little endian */
+	SDatasourceSetByteOrder(ds, S_BYTEORDER_LE, error);
+	if (S_CHK_ERR(error, S_CONTERR,
+		"s_write_audio_riff_16",
+		"Call to \"SDatasourceSetByteOrder\" failed"))
+		goto quit_error;
+
+	/* write header */
+	info = "RIFF";
+	SDatasourceWrite(ds, info, sizeof(char), 4, error);
+	if (S_CHK_ERR(error, S_CONTERR,
+		"s_write_audio_riff_16",
+		"Call to \"SDatasourceWrite\" failed"))
+		goto quit_error;
+
+	/* num bytes in whole file */
+	dword = (audio->num_samples * S_AUDIO_NUM_CHANNELS * sizeof(sint16)) + 8 + 16 + 12;
+	s_uint32_write(ds, dword, error);
+	if (S_CHK_ERR(error, S_CONTERR,
+		"s_write_audio_riff_16",
+		"Call to \"s_uint32_write\" failed"))
+		goto quit_error;
+
+	info = "WAVE";
+	SDatasourceWrite(ds, info, sizeof(char), 4, error);
+	if (S_CHK_ERR(error, S_CONTERR,
+		"s_write_audio_riff_16",
+		"Call to \"SDatasourceWrite\" failed"))
+		goto quit_error;
+
+	info = "fmt ";
+	SDatasourceWrite(ds, info, sizeof(char), 4, error);
+	if (S_CHK_ERR(error, S_CONTERR,
+		"s_write_audio_riff_16",
+		"Call to \"SDatasourceWrite\" failed"))
+		goto quit_error;
+
+	/* size of header */
+	dword = 16;
+	s_uint32_write(ds, dword, error);
+	if (S_CHK_ERR(error, S_CONTERR,
+		"s_write_audio_riff_16",
+		"Call to \"s_uint32_write\" failed"))
+		goto quit_error;
+
+	/* format type */
+	word = RIFF_FORMAT_PCM;
+	s_uint16_write(ds, word, error);
+	if (S_CHK_ERR(error, S_CONTERR,
+		"s_write_audio_riff_16",
+		"Call to \"s_uint16_write\" failed"))
+		goto quit_error;
+
+	/* number of channels */
+	word = S_AUDIO_NUM_CHANNELS;
+	s_uint16_write(ds, word, error);
+	if (S_CHK_ERR(error, S_CONTERR,
+		"s_write_audio_riff_16",
+		"Call to \"s_uint16_write\" failed"))
+		goto quit_error;
+
+	/* sample rate */
+	dword = audio->sample_rate;
+	s_uint32_write(ds, dword, error);
+	if (S_CHK_ERR(error, S_CONTERR,
+		"s_write_audio_riff_16",
+		"Call to \"s_uint32_write\" failed"))
+		goto quit_error;
+
+	/* average bytes per second */
+	dword = (audio->sample_rate * S_AUDIO_NUM_CHANNELS * sizeof(sint16));
+	s_uint32_write(ds, dword, error);
+	if (S_CHK_ERR(error, S_CONTERR,
+		"s_write_audio_riff_16",
+		"Call to \"s_uint32_write\" failed"))
+		goto quit_error;
+
+	/* block align */
+	word = S_AUDIO_NUM_CHANNELS * sizeof(sint16);
+	s_uint16_write(ds, word, error);
+	if (S_CHK_ERR(error, S_CONTERR,
+		"s_write_audio_riff_16",
+		"Call to \"s_uint16_write\" failed"))
+		goto quit_error;
+
+	/* bits per sample */
+	word = sizeof(sint16) * 8;
+	s_uint16_write(ds, word, error);
+	if (S_CHK_ERR(error, S_CONTERR,
+		"s_write_audio_riff_16",
+		"Call to \"s_uint16_write\" failed"))
+		goto quit_error;
+
+	info = "data";
+	SDatasourceWrite(ds, info, sizeof(char), 4, error);
+	if (S_CHK_ERR(error, S_CONTERR,
+		"s_write_audio_riff_16",
+		"Call to \"SDatasourceWrite\" failed"))
+		goto quit_error;
+
+	/* number of bytes in data */
+	dword = (audio->num_samples * S_AUDIO_NUM_CHANNELS * sizeof(sint16));
+	s_uint32_write(ds, dword, error);
+	if (S_CHK_ERR(error, S_CONTERR,
+		"s_write_audio_riff_16",
+		"Call to \"s_uint32_write\" failed"))
+		goto quit_error;
+
+	/* get the audio samples in 16 bit integer format */
+	samples = S_CALLOC(sint16, audio->num_samples * S_AUDIO_NUM_CHANNELS);
+	for (counter = 0; counter < audio->num_samples; counter++)
+		samples[counter] = (sint16)audio->samples[counter];
+
+	/* Do byte swap if big-endian */
+#ifdef SPCT_BIG_ENDIAN
+	samples = (sint16*)s_swap_bytes16((uint16*)samples, audio->num_samples * S_AUDIO_NUM_CHANNELS, error);
+	if (S_CHK_ERR(error, S_CONTERR,
+		"s_write_audio_riff_16",
+		"Call to \"s_swap_bytes16\" failed"))
+		goto quit_error;
+#endif
+
+	/* write audio to file */
+	SDatasourceWrite(ds, samples, sizeof(sint16), audio->num_samples * S_AUDIO_NUM_CHANNELS, error);
+	if (S_CHK_ERR(error, S_CONTERR,
+		"s_write_audio_riff_16",
+		"Call to \"SDatasourceWrite\" failed"))
+		goto quit_error;
+
+	/* done */
+	goto quit;
+
+	/* errors and normal clean up code here */
+quit_error:
+quit:
+	if (samples != NULL)
+		S_FREE(samples);
+
+	if (ds != NULL)
+		S_DELETE(ds, "s_write_audio_riff_16_to_datasource", error);
 }

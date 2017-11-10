2522a52a1f725d596e9a3aabb8a881a5369c4b2f
Giulio Paci <giuliopaci@gmail.com>
2017-11-10 08:35:18 +0100

    Use custom variables for source and binary directories.
    This let speect to be compiled in a subdirectory of another project.

--- engine/cmake/engineVersion.cmake
+++ engine/cmake/engineVersion.cmake
@@ -17,5 +17,5 @@
 
 # Note that the version variables are set in speect/cmake/spctVersion.cmake
 
-configure_file(${CMAKE_SOURCE_DIR}/engine/config/version.h.in 
-  ${CMAKE_BINARY_DIR}/engine/src/include/version.h)
+configure_file(${CMAKE_SPEECT_SOURCE_DIR}/engine/config/version.h.in 
+  ${CMAKE_SPEECT_BINARY_DIR}/engine/src/include/version.h)

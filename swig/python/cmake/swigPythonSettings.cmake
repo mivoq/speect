b0c2306a324ac74b69d68a8d5a8ae5ff554fb470
Giulio Paci <giuliopaci@gmail.com>
2017-11-10 08:35:19 +0100

    Use custom variables for source and binary directories.
    This let speect to be compiled in a subdirectory of another project.

--- swig/python/cmake/swigPythonSettings.cmake
+++ swig/python/cmake/swigPythonSettings.cmake
@@ -15,6 +15,6 @@
 #                    Speect Python logging configuration file                        #
 #------------------------------------------------------------------------------------#
 
-set(SPCT_PYTHON_LOG_CONFIG_FILE "${CMAKE_BINARY_DIR}/spct_python_log.conf")
+set(SPCT_PYTHON_LOG_CONFIG_FILE "${CMAKE_SPEECT_BINARY_DIR}/spct_python_log.conf")
 mark_as_advanced(SPCT_PYTHON_LOG_CONFIG_FILE)
 

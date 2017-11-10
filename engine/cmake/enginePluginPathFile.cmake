b69753349e648743883d3250e47bc4e097311c78
Giulio Paci <giuliopaci@gmail.com>
2017-11-10 08:35:18 +0100

    Use custom variables for source and binary directories.
    This let speect to be compiled in a subdirectory of another project.

--- engine/cmake/enginePluginPathFile.cmake
+++ engine/cmake/enginePluginPathFile.cmake
@@ -16,19 +16,19 @@
 #------------------------------------------------------------------------------------#
 
 # Copy plugin.lib file to binary dir
-speect_file_copy(${CMAKE_SOURCE_DIR}/engine/config/plugin.lib ${CMAKE_BINARY_DIR}/plugins/lib/plugin.lib)
+speect_file_copy(${CMAKE_SPEECT_SOURCE_DIR}/engine/config/plugin.lib ${CMAKE_SPEECT_BINARY_DIR}/plugins/lib/plugin.lib)
 
 
 #------------------------------------------------------------------------------------#
 #                  Configure the speect.conf installation script                     #
 #------------------------------------------------------------------------------------#
 
-configure_file(${CMAKE_SOURCE_DIR}/engine/config/plugin_path_install.cmake.in 
-  ${CMAKE_BINARY_DIR}/engine/cmake/plugin_path_install.cmake  @ONLY)
+configure_file(${CMAKE_SPEECT_SOURCE_DIR}/engine/config/plugin_path_install.cmake.in 
+  ${CMAKE_SPEECT_BINARY_DIR}/engine/cmake/plugin_path_install.cmake  @ONLY)
 
 
 #------------------------------------------------------------------------------------#
 #                   Set the installation script (UNIX only)                          #
 #------------------------------------------------------------------------------------#
 
-install(SCRIPT ${CMAKE_BINARY_DIR}/engine/cmake/plugin_path_install.cmake)
+install(SCRIPT ${CMAKE_SPEECT_BINARY_DIR}/engine/cmake/plugin_path_install.cmake)

c20b5761ed8bac47e68a34a3d4c3356d80bdd40f
Giulio Paci <giuliopaci@gmail.com>
2017-12-23 15:13:54 +0100

    Enable platform specific path abstractions on all platforms.

--- engine/cmake/engineAbstractionPaths.cmake
+++ engine/cmake/engineAbstractionPaths.cmake
@@ -28,10 +28,11 @@ set(SPCT_INCLUDE_POSIX_PATH_FILES
   posix/posix_path.h
 )
 
+list(APPEND SPCT_LIBRARY_SOURCES "${CMAKE_SPEECT_SOURCE_DIR}/engine/src/base/utils/platform/${SPCT_SRC_POSIX_PATH_FILES}")
+
 if(SPCT_UNIX OR SPCT_MACOSX)
   set(SPCT_PATH_SPECIFIC_IMPL "${SPCT_INCLUDE_POSIX_PATH_FILES}")
   set(SPCT_PATH_SPECIFIC_IMPL_DIR "posix")
-  list(APPEND SPCT_LIBRARY_SOURCES "${CMAKE_SPEECT_SOURCE_DIR}/engine/src/base/utils/platform/${SPCT_SRC_POSIX_PATH_FILES}")
 
   set(SPCT_PATH_SUPPORT 1)
 endif(SPCT_UNIX OR SPCT_MACOSX)
@@ -49,11 +50,11 @@ set(SPCT_INCLUDE_WIN32_PATH_FILES
   win32/win32_path.h
 )
 
+list(APPEND SPCT_LIBRARY_SOURCES "${CMAKE_SPEECT_SOURCE_DIR}/engine/src/base/utils/platform/${SPCT_SRC_WIN32_PATH_FILES}")
 
 if(SPCT_WIN32)
   set(SPCT_PATH_SPECIFIC_IMPL "${SPCT_INCLUDE_WIN32_PATH_FILES}")
   set(SPCT_PATH_SPECIFIC_IMPL_DIR "win32")
-  list(APPEND SPCT_LIBRARY_SOURCES "${CMAKE_SPEECT_SOURCE_DIR}/engine/src/base/utils/platform/${SPCT_SRC_WIN32_PATH_FILES}")
 
   set(SPCT_PATH_SUPPORT 1)
 endif(SPCT_WIN32)
@@ -71,8 +72,11 @@ configure_file(${CMAKE_SPEECT_SOURCE_DIR}/engine/config/path_impl.h.in
 #                               installation                                         #
 #------------------------------------------------------------------------------------#
 
-install(FILES ${CMAKE_SPEECT_SOURCE_DIR}/engine/src/base/utils/platform/${SPCT_PATH_SPECIFIC_IMPL}
-  DESTINATION include/speect/engine/base/utils/platform/${SPCT_PATH_SPECIFIC_IMPL_DIR}/)
+install(FILES ${CMAKE_SPEECT_SOURCE_DIR}/engine/src/base/utils/platform/${SPCT_INCLUDE_POSIX_PATH_FILES}
+  DESTINATION include/speect/engine/base/utils/platform/posix/)
+
+install(FILES ${CMAKE_SPEECT_SOURCE_DIR}/engine/src/base/utils/platform/${SPCT_INCLUDE_WIN32_PATH_FILES}
+  DESTINATION include/speect/engine/base/utils/platform/win32/)
 
 install(FILES ${CMAKE_SPEECT_BINARY_DIR}/engine/src/base/utils/platform/path_impl.h
   DESTINATION include/speect/engine/base/utils/platform)

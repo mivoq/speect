f5381adb227613cb1403c03904288cbbe5d87cf8
Giulio Paci <giuliopaci@gmail.com>
2018-03-11 12:15:38 +0100

    Simplified engineAbstractionPaths.cmake.

--- engine/cmake/engineAbstractionPaths.cmake
+++ engine/cmake/engineAbstractionPaths.cmake
@@ -11,10 +11,7 @@
 ######################################################################################
 
 # SPCT_PATH_SPECIFIC_IMPL_INCLUDES : list of implementation include files
-# SPCT_PATH_SPECIFIC_IMPL_DIR      : implementation directory
-
-
-set(SPCT_PATH_SUPPORT 0)
+# SPCT_PATH_SPECIFIC_IMPL_VARIANT  : implementation identifier
 
 #------------------------------------------------------------------------------------#
 #                              POSIX implementation                                  #
@@ -30,12 +27,8 @@ set(SPCT_INCLUDE_POSIX_PATH_FILES
 
 list(APPEND SPCT_LIBRARY_SOURCES "${CMAKE_SPEECT_SOURCE_DIR}/engine/src/base/utils/platform/${SPCT_SRC_POSIX_PATH_FILES}")
 
-if(SPCT_UNIX OR SPCT_MACOSX)
-  set(SPCT_PATH_SPECIFIC_IMPL "${SPCT_INCLUDE_POSIX_PATH_FILES}")
-  set(SPCT_PATH_SPECIFIC_IMPL_DIR "posix")
-
-  set(SPCT_PATH_SUPPORT 1)
-endif(SPCT_UNIX OR SPCT_MACOSX)
+install(FILES ${CMAKE_SPEECT_SOURCE_DIR}/engine/src/base/utils/platform/${SPCT_INCLUDE_POSIX_PATH_FILES}
+  DESTINATION include/speect/engine/base/utils/platform/posix/)
 
 
 #------------------------------------------------------------------------------------#
@@ -52,39 +45,24 @@ set(SPCT_INCLUDE_WIN32_PATH_FILES
 
 list(APPEND SPCT_LIBRARY_SOURCES "${CMAKE_SPEECT_SOURCE_DIR}/engine/src/base/utils/platform/${SPCT_SRC_WIN32_PATH_FILES}")
 
+install(FILES ${CMAKE_SPEECT_SOURCE_DIR}/engine/src/base/utils/platform/${SPCT_INCLUDE_WIN32_PATH_FILES}
+  DESTINATION include/speect/engine/base/utils/platform/win32/)
+
+#------------------------------------------------------------------------------------#
+#                              Default implementation                                #
+#------------------------------------------------------------------------------------#
+
 if(SPCT_WIN32)
   set(SPCT_PATH_SPECIFIC_IMPL "${SPCT_INCLUDE_WIN32_PATH_FILES}")
-  set(SPCT_PATH_SPECIFIC_IMPL_DIR "win32")
-
-  set(SPCT_PATH_SUPPORT 1)
-endif(SPCT_WIN32)
-
-
-#------------------------------------------------------------------------------------#
-#                               engine header                                        #
-#------------------------------------------------------------------------------------#
+  set(SPCT_PATH_SPECIFIC_IMPL_VARIANT "win32")
+else()
+  set(SPCT_PATH_SPECIFIC_IMPL "${SPCT_INCLUDE_POSIX_PATH_FILES}")
+  set(SPCT_PATH_SPECIFIC_IMPL_VARIANT "posix")
+endif()
 
 configure_file(${CMAKE_SPEECT_SOURCE_DIR}/engine/config/path_impl.h.in 
   ${CMAKE_SPEECT_BINARY_DIR}/engine/src/base/utils/platform/path_impl.h)
 
-
-#------------------------------------------------------------------------------------#
-#                               installation                                         #
-#------------------------------------------------------------------------------------#
-
-install(FILES ${CMAKE_SPEECT_SOURCE_DIR}/engine/src/base/utils/platform/${SPCT_INCLUDE_POSIX_PATH_FILES}
-  DESTINATION include/speect/engine/base/utils/platform/posix/)
-
-install(FILES ${CMAKE_SPEECT_SOURCE_DIR}/engine/src/base/utils/platform/${SPCT_INCLUDE_WIN32_PATH_FILES}
-  DESTINATION include/speect/engine/base/utils/platform/win32/)
-
 install(FILES ${CMAKE_SPEECT_BINARY_DIR}/engine/src/base/utils/platform/path_impl.h
   DESTINATION include/speect/engine/base/utils/platform)
 
-#------------------------------------------------------------------------------------#
-#                          Error if no implementation                                #
-#------------------------------------------------------------------------------------#
-
-if(NOT SPCT_PATH_SUPPORT)
-  message(FATAL_ERROR "Unknown file system paths support.")    
-endif(NOT SPCT_PATH_SUPPORT)
